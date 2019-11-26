e1cc1b2f8c71b2bafb5b2f475000448ba077d9bd
Kumar Atul <kumar@clevertap.com>
2019-11-26 17:01:17 +0530

    Added other template support for android

--- clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/ads/AdConstants.java
+++ clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/ads/AdConstants.java
@@ -1,11 +1,16 @@
 package com.clevertap.android.sdk.ads;
 
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 
 public interface AdConstants {
 
     enum CtAdType {
-
+        SIMPLE("simple"),
+        SIMPLE_WITH_IMAGE("simple-image"),
+        CAROUSEL("carousel"),
+        CAROUSEL_WITH_IMAGE("carousel-image"),
+        MESSAGE_WITH_ICON("message-icon"),
         CUSTOM_KEY_VALUE("custom-key-value");
 
         private final String adType;
@@ -16,8 +21,22 @@ public interface AdConstants {
 
         public static CtAdType type(String type) {
 
-            if ("custom-key-value".equalsIgnoreCase(type)) {
-                return CUSTOM_KEY_VALUE;
+            if (TextUtils.isEmpty(type))
+                return null;
+
+            switch (type) {
+                case "simple":
+                    return SIMPLE;
+                case "simple-image":
+                    return SIMPLE_WITH_IMAGE;
+                case "carousel":
+                    return CAROUSEL;
+                case "carousel-image":
+                    return CAROUSEL_WITH_IMAGE;
+                case "message-icon":
+                    return MESSAGE_WITH_ICON;
+                case "custom-key-value":
+                    return CUSTOM_KEY_VALUE;
             }
             return null;
         }
--- clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/ads/model/CTAdUnitContent.java
+++ clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/ads/model/CTAdUnitContent.java
@@ -1,4 +1,258 @@
 package com.clevertap.android.sdk.ads.model;
 
-public class CTAdUnitContent {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.clevertap.android.sdk.Logger;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Content class for holding AdContent Data
+ */
+public class CTAdUnitContent implements Parcelable {
+    private String title;
+    private String titleColor;
+    private String message;
+    private String messageColor;
+    private String media;
+    private String contentType;
+    private String posterUrl;
+    private String actionUrl;
+    private String icon;
+    private Boolean hasUrl;
+
+    private CTAdUnitContent(String title, String titleColor, String message, String messageColor,
+                            String icon, String media, String contentType, String posterUrl,
+                            String actionUrl, boolean hasUrl) {
+        this.title = title;
+        this.titleColor = titleColor;
+        this.message = message;
+        this.messageColor = messageColor;
+        this.icon = icon;
+        this.media = media;
+        this.contentType = contentType;
+        this.posterUrl = posterUrl;
+        this.actionUrl = actionUrl;
+        this.hasUrl = hasUrl;
+    }
+
+
+    private CTAdUnitContent(Parcel in) {
+        title = in.readString();
+        titleColor = in.readString();
+        message = in.readString();
+        messageColor = in.readString();
+        icon = in.readString();
+        media = in.readString();
+        contentType = in.readString();
+        posterUrl = in.readString();
+        actionUrl = in.readString();
+        hasUrl = in.readByte() != 0x00;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(titleColor);
+        dest.writeString(message);
+        dest.writeString(messageColor);
+        dest.writeString(icon);
+        dest.writeString(media);
+        dest.writeString(contentType);
+        dest.writeString(posterUrl);
+        dest.writeString(actionUrl);
+        dest.writeByte((byte) (hasUrl ? 0x01 : 0x00));
+    }
+
+    static CTAdUnitContent toContent(JSONObject contentObject) {
+        try {
+            String title = "", titleColor = "", message = "", messageColor = "",
+                    icon = "", media = "", contentType = "", posterUrl = "",
+                    actionUrl = "";
+
+            boolean hasUrl = false;
+
+
+            JSONObject titleObject = contentObject.has("title") ? contentObject.getJSONObject("title") : null;
+            if (titleObject != null) {
+                title = titleObject.has("text") ? titleObject.getString("text") : "";
+                titleColor = titleObject.has("color") ? titleObject.getString("color") : "";
+            }
+            JSONObject msgObject = contentObject.has("message") ? contentObject.getJSONObject("message") : null;
+            if (msgObject != null) {
+                message = msgObject.has("text") ? msgObject.getString("text") : "";
+                messageColor = msgObject.has("color") ? msgObject.getString("color") : "";
+            }
+            JSONObject iconObject = contentObject.has("icon") ? contentObject.getJSONObject("icon") : null;
+            if (iconObject != null) {
+                icon = iconObject.has("url") ? iconObject.getString("url") : "";
+            }
+            JSONObject mediaObject = contentObject.has("media") ? contentObject.getJSONObject("media") : null;
+            if (mediaObject != null) {
+                media = mediaObject.has("url") ? mediaObject.getString("url") : "";
+                contentType = mediaObject.has("content_type") ? mediaObject.getString("content_type") : "";
+                posterUrl = mediaObject.has("poster") ? mediaObject.getString("poster") : "";
+            }
+
+            JSONObject actionObject = contentObject.has("action") ? contentObject.getJSONObject("action") : null;
+            if (actionObject != null) {
+                hasUrl = actionObject.has("hasUrl") && actionObject.getBoolean("hasUrl");
+                JSONObject urlObject = actionObject.has("url") ? actionObject.getJSONObject("url") : null;
+                if (urlObject != null && hasUrl) {
+                    JSONObject androidObject = urlObject.has("android") ? urlObject.getJSONObject("android") : null;
+                    if (androidObject != null) {
+                        actionUrl = androidObject.has("text") ? androidObject.getString("text") : "";
+                    }
+                }
+            }
+
+            new CTAdUnitContent(title, titleColor, message, messageColor,
+                    icon, media, contentType, posterUrl,
+                    actionUrl, hasUrl);
+
+        } catch (JSONException e) {
+            Logger.v("Unable to init CTInboxMessageContent with JSON - " + e.getLocalizedMessage());
+        }
+        return null;
+    }
+
+    /**
+     * Returns the title section of the AdUnit
+     *
+     * @return String
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Returns the message section of the AdUnit
+     *
+     * @return String
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Returns the media URL of the AdUnit
+     *
+     * @return String
+     */
+    public String getMedia() {
+        return media;
+    }
+
+    /**
+     * Return the action URL of the body of the AdUnit
+     *
+     * @return String
+     */
+    public String getActionUrl() {
+        return actionUrl;
+    }
+
+    /**
+     * Returns the URL as String for the icon in case of Icon Message template
+     *
+     * @return String
+     */
+    public String getIcon() {
+        return icon;
+    }
+
+    /**
+     * Returns the hexcode value of the title color as String
+     *
+     * @return String
+     */
+    public String getTitleColor() {
+        return titleColor;
+    }
+
+    /**
+     * Returns the hexcode value of the message color as String
+     *
+     * @return String
+     */
+    public String getMessageColor() {
+        return messageColor;
+    }
+
+    /**
+     * Returns URL for the thumbnail of the video
+     *
+     * @return String
+     */
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    /**
+     * Returns the content type of the media
+     *
+     * @return String
+     */
+    public String getContentType() {
+        return contentType;
+    }
+
+    /**
+     * Method to check whether media in the {@link CTAdUnitContent} object is an image.
+     *
+     * @return true if the media type is image
+     * false if the media type is not an image
+     */
+    public boolean mediaIsImage() {
+        return contentType != null && this.media != null && contentType.startsWith("image") && !contentType.equals("image/gif");
+    }
+
+    /**
+     * Method to check whether media in the {@link CTAdUnitContent} object is an GIF.
+     *
+     * @return true if the media type is GIF
+     * false if the media type is not an GIF
+     */
+    public boolean mediaIsGIF() {
+        return contentType != null && this.media != null && contentType.equals("image/gif");
+    }
+
+    /**
+     * Method to check whether media in the {@link CTAdUnitContent} object is a video.
+     *
+     * @return true if the media type is video
+     * false if the media type is not a video
+     */
+    public boolean mediaIsVideo() {
+        return contentType != null && this.media != null && contentType.startsWith("video");
+    }
+
+    /**
+     * Method to check whether media in the {@link CTAdUnitContent} object is an audio.
+     *
+     * @return true if the media type is audio
+     * false if the media type is not an audio
+     */
+    public boolean mediaIsAudio() {
+        return contentType != null && this.media != null && contentType.startsWith("audio");
+    }
+
+    public static final Creator<CTAdUnitContent> CREATOR = new Creator<CTAdUnitContent>() {
+        @Override
+        public CTAdUnitContent createFromParcel(Parcel in) {
+            return new CTAdUnitContent(in);
+        }
+
+        @Override
+        public CTAdUnitContent[] newArray(int size) {
+            return new CTAdUnitContent[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 }
