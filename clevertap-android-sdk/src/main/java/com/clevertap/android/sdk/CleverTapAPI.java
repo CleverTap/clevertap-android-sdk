85569e2e1f40fd4729fdea87363bfd761e930de2
Kumar Atul <kumar@clevertap.com>
2020-06-26 23:27:21 +0530

    chores:bitrise build issue

--- clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/CleverTapAPI.java
+++ clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/CleverTapAPI.java
@@ -55,7 +55,6 @@ import com.clevertap.android.sdk.featureFlags.FeatureFlagListener;
 import com.clevertap.android.sdk.product_config.CTProductConfigController;
 import com.clevertap.android.sdk.product_config.CTProductConfigControllerListener;
 import com.clevertap.android.sdk.product_config.CTProductConfigListener;
-import com.google.android.gms.plus.model.people.Person;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 
@@ -4252,118 +4251,12 @@ public class CleverTapAPI implements CTInAppNotification.CTInAppNotificationList
     }
 
     /**
-     * Pushes everything useful within the Google Plus
-     * {@link com.google.android.gms.plus.model.people.Person} object.
-     *
-     * @param person The {@link com.google.android.gms.plus.model.people.Person} object
-     * @see com.google.android.gms.plus.model.people.Person
-     */
-    @SuppressWarnings({"all"})
-    public void pushGooglePlusPerson(final com.google.android.gms.plus.model.people.Person person) {
-        postAsyncSafely("pushGooglePlusPerson", new Runnable() {
-            @Override
-            public void run() {
-                _pushGooglePlusPerson(person);
-            }
-        });
-    }
-
-    @SuppressWarnings("all")
-    private void _pushGooglePlusPerson(com.google.android.gms.plus.model.people.Person person) {
-        if (person == null) {
-            return;
-        }
-        try {
-            // Note: No validations are required here, as everything is controlled
-            String name = "";
-            if (person.hasDisplayName()) {
-                try {
-                    // Certain users have nasty looking names - unicode chars, validate for any
-                    // not allowed chars
-                    name = person.getDisplayName();
-                    ValidationResult vr = validator.cleanObjectValue(name, Validator.ValidationContext.Profile);
-                    name = vr.getObject().toString();
-
-                    if (vr.getErrorCode() != 0) {
-                        pushValidationResult(vr);
-                    }
-                } catch (Throwable t) {
-                    // Weird name, wasn't a string, or any number
-                    // This would never happen with G+
-                    name = "";
-                }
-            }
-
-            String gender = null;
-            if (person.hasGender()) {
-                if (person.getGender() == com.google.android.gms.plus.model.people.Person.Gender.MALE) {
-                    gender = "M";
-                } else if (person.getGender() == com.google.android.gms.plus.model.people.Person.Gender.FEMALE) {
-                    gender = "F";
-                }
-            }
-
-            String birthday = null;
-
-            if (person.hasBirthday()) {
-                // We have the string as YYYY-MM-DD
-                try {
-                    Date date = Constants.GP_DOB_DATE_FORMAT.parse(person.getBirthday());
-                    birthday = "$D_" + (int) (date.getTime() / 1000);
-                } catch (Throwable t) {
-                    // Differs from the specs
-                    birthday = null;
-                }
-            }
-
-            String work = null;
-            if (person.hasOrganizations()) {
-                List<Person.Organizations> organizations = person.getOrganizations();
-                for (com.google.android.gms.plus.model.people.Person.Organizations o : organizations) {
-                    if (o.getType() == com.google.android.gms.plus.model.people.Person.Organizations.Type.WORK) {
-                        work = "Y";
-                        break;
-                    }
-                }
-            }
-
-            String id = "";
-            if (person.hasId()) {
-                id = person.getId();
-            }
-
-            String married = null;
-            if (person.hasRelationshipStatus()) {
-                if (person.getRelationshipStatus() == com.google.android.gms.plus.model.people.Person.RelationshipStatus.MARRIED) {
-                    married = "Y";
-                } else {
-                    married = "N";
-                }
-            }
-
-            // Construct json object from the data
-            final JSONObject profile = new JSONObject();
-            if (id != null && id.trim().length() > 0) profile.put("GPID", id);
-            if (name != null && name.trim().length() > 0) profile.put("Name", name);
-            if (gender != null && gender.trim().length() > 0) profile.put("Gender", gender);
-            if (work != null && work.trim().length() > 0) profile.put("Employed", work);
-            if (birthday != null && birthday.trim().length() > 4) profile.put("DOB", birthday);
-            if (married != null && married.trim().length() > 0) profile.put("Married", married);
-
-            pushBasicProfile(profile);
-        } catch (Throwable t) {
-            // We won't get here
-            getConfigLogger().verbose(getAccountId(), "FATAL: Creating G+ profile update event failed!");
-        }
-    }
-
-    /**
      * Return the user profile property value for the specified key
      *
      * @param name String
      * @return {@link JSONArray}, String or null
      */
-    @SuppressWarnings({"unused"})
+//    @SuppressWarnings({"unused"})
     public Object getProperty(String name) {
         if (!this.config.isPersonalizationEnabled()) return null;
         return getLocalDataStore().getProfileProperty(name);
