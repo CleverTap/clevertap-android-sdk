71a59c33e316e3d73baf75a6076a3eb05c60896c
Kumar Atul <kumar@clevertap.com>
2020-06-30 11:57:16 +0530

    chores(test): Added test account.

--- clevertap-android-sdk/src/test/java/com/clevertap/android/sdk/Constant.java
+++ clevertap-android-sdk/src/test/java/com/clevertap/android/sdk/Constant.java
@@ -1,6 +1,6 @@
 package com.clevertap.android.sdk;
 
 public interface Constant {
-    String ACC_ID = "ZWW-WWW-WWRZ";
-    String ACC_TOKEN = "000-001";
+    String ACC_ID = "R65-RR9-9R5Z";
+    String ACC_TOKEN = "c22-562";
 }
