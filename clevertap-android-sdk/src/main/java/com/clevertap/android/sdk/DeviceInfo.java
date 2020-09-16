d2620a5f41b5808ca38bc744cfb8e351a464998f
Darshan Pania <darshan@clevertap.com>
2020-09-16 16:03:42 +0530

    fix: Handle Security Exception in getNetworkType for Android 11 devices SDK-411

--- clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/DeviceInfo.java
+++ clevertap-android-sdk/src/main/java/com/clevertap/android/sdk/DeviceInfo.java
@@ -610,33 +610,39 @@ class DeviceInfo {
         }
 
         private String getNetworkType() {
-            TelephonyManager mTelephonyManager = (TelephonyManager)
-                    context.getSystemService(Context.TELEPHONY_SERVICE);
-            if (mTelephonyManager == null) {
-                return null;
-            }
-            int networkType = mTelephonyManager.getNetworkType();
-            switch (networkType) {
-                case TelephonyManager.NETWORK_TYPE_GPRS:
-                case TelephonyManager.NETWORK_TYPE_EDGE:
-                case TelephonyManager.NETWORK_TYPE_CDMA:
-                case TelephonyManager.NETWORK_TYPE_1xRTT:
-                case TelephonyManager.NETWORK_TYPE_IDEN:
-                    return "2G";
-                case TelephonyManager.NETWORK_TYPE_UMTS:
-                case TelephonyManager.NETWORK_TYPE_EVDO_0:
-                case TelephonyManager.NETWORK_TYPE_EVDO_A:
-                case TelephonyManager.NETWORK_TYPE_HSDPA:
-                case TelephonyManager.NETWORK_TYPE_HSUPA:
-                case TelephonyManager.NETWORK_TYPE_HSPA:
-                case TelephonyManager.NETWORK_TYPE_EVDO_B:
-                case TelephonyManager.NETWORK_TYPE_EHRPD:
-                case TelephonyManager.NETWORK_TYPE_HSPAP:
-                    return "3G";
-                case TelephonyManager.NETWORK_TYPE_LTE:
-                    return "4G";
-                default:
+            try{
+                TelephonyManager mTelephonyManager = (TelephonyManager)
+                        context.getSystemService(Context.TELEPHONY_SERVICE);
+                if (mTelephonyManager == null) {
                     return null;
+                }
+
+                int networkType = mTelephonyManager.getNetworkType();
+                switch (networkType) {
+                    case TelephonyManager.NETWORK_TYPE_GPRS:
+                    case TelephonyManager.NETWORK_TYPE_EDGE:
+                    case TelephonyManager.NETWORK_TYPE_CDMA:
+                    case TelephonyManager.NETWORK_TYPE_1xRTT:
+                    case TelephonyManager.NETWORK_TYPE_IDEN:
+                        return "2G";
+                    case TelephonyManager.NETWORK_TYPE_UMTS:
+                    case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                    case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                    case TelephonyManager.NETWORK_TYPE_HSDPA:
+                    case TelephonyManager.NETWORK_TYPE_HSUPA:
+                    case TelephonyManager.NETWORK_TYPE_HSPA:
+                    case TelephonyManager.NETWORK_TYPE_EVDO_B:
+                    case TelephonyManager.NETWORK_TYPE_EHRPD:
+                    case TelephonyManager.NETWORK_TYPE_HSPAP:
+                        return "3G";
+                    case TelephonyManager.NETWORK_TYPE_LTE:
+                        return "4G";
+                    default:
+                        return null;
+                }
+            }catch (SecurityException se){
+                Logger.d("Caught Security Exception, returning Network Type as NULL");
+                return null;
             }
         }
 
