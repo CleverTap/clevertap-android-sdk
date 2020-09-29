1436ee6a88289b100e8ea515c2f6b48233bb68c6
Darshan Pania <darshan@clevertap.com>
2020-09-30 00:52:52 +0530

    chore(docs): Added all READMEs, fixed network type cases SDK-84

--- clevertap-core/src/main/java/com/clevertap/android/sdk/Utils.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/Utils.java
@@ -276,20 +276,24 @@ public final class Utils {
         }
 
         switch (networkType) {
-            case TelephonyManager.NETWORK_TYPE_CDMA:
-                return "CDMA";
-            case TelephonyManager.NETWORK_TYPE_EDGE:
-                return "EDGE";
             case TelephonyManager.NETWORK_TYPE_GPRS:
-                return "GPRS";
-            case TelephonyManager.NETWORK_TYPE_HSDPA:
-            case TelephonyManager.NETWORK_TYPE_HSPA:
-            case TelephonyManager.NETWORK_TYPE_HSPAP:
-            case TelephonyManager.NETWORK_TYPE_HSUPA:
+            case TelephonyManager.NETWORK_TYPE_EDGE:
+            case TelephonyManager.NETWORK_TYPE_CDMA:
+            case TelephonyManager.NETWORK_TYPE_1xRTT:
+            case TelephonyManager.NETWORK_TYPE_IDEN:
+                return "2G";
             case TelephonyManager.NETWORK_TYPE_UMTS:
+            case TelephonyManager.NETWORK_TYPE_EVDO_0:
+            case TelephonyManager.NETWORK_TYPE_EVDO_A:
+            case TelephonyManager.NETWORK_TYPE_HSDPA:
+            case TelephonyManager.NETWORK_TYPE_HSUPA:
+            case TelephonyManager.NETWORK_TYPE_HSPA:
+            case TelephonyManager.NETWORK_TYPE_EVDO_B:
+            case TelephonyManager.NETWORK_TYPE_EHRPD:
+            case TelephonyManager.NETWORK_TYPE_HSPAP:
                 return "3G";
             case TelephonyManager.NETWORK_TYPE_LTE:
-                return "LTE";
+                return "4G";
             case TelephonyManager.NETWORK_TYPE_NR:
                 return "5G";
             default:
