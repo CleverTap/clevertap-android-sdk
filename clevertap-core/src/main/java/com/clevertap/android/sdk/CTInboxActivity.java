a3059aac21f5a0d864e57496763e266ec2944857
Vikash Kumar <kumarv4624@gmail.com>
2020-12-15 21:37:58 +0530

    Update CTInboxActivity.java

--- clevertap-core/src/main/java/com/clevertap/android/sdk/CTInboxActivity.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/CTInboxActivity.java
@@ -243,4 +243,5 @@ public class CTInboxActivity extends FragmentActivity implements CTInboxListView
 
     private String getFragmentTag() {
         return config.getAccountId() + ":CT_INBOX_LIST_VIEW_FRAGMENT";
-    }}
+    }
+}
