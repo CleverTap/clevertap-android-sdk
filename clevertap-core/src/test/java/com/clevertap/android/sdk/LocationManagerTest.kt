e1b157d07de62900db6163b072c77e24f7183ff6
Piyush Kukadiya <piyush.kukadiya@clevertap.com>
2021-02-03 16:17:54 +0530

    task(TDD): add unit tests for _getLocation() in LocationManager SDK-628

--- clevertap-core/src/test/java/com/clevertap/android/sdk/LocationManagerTest.kt
+++ clevertap-core/src/test/java/com/clevertap/android/sdk/LocationManagerTest.kt
@@ -1,12 +1,13 @@
 package com.clevertap.android.sdk
 
+import android.content.Context
 import android.location.Location
 import com.clevertap.android.shared.test.BaseTestCase
 import org.junit.*
 import org.junit.runner.*
-import org.mockito.*
 import org.mockito.Mockito.*
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows.shadowOf
 import java.util.concurrent.Future
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
@@ -25,11 +26,13 @@ class LocationManagerTest : BaseTestCase() {
     override fun setUp() {
         super.setUp()
         coreMetaData = CoreMetaData()
-        eventQueueManager = Mockito.mock(EventQueueManager::class.java)
+        eventQueueManager = mock(EventQueueManager::class.java)
         locationManager = spy(LocationManager(application, cleverTapInstanceConfig, coreMetaData, eventQueueManager))
-        location = Location("")
-        location.latitude = 17.355
-        location.longitude = 7.355
+        location = Location("").apply {
+            latitude = 17.355
+            longitude = 7.355
+            accuracy = 50f
+        }
     }
 
     @Test
@@ -111,5 +114,55 @@ class LocationManagerTest : BaseTestCase() {
         verify(eventQueueManager).queueEvent(any(), any(), anyInt())
         assertNotNull(futureActual)
         assertEquals(locationManager.lastLocationPingTime, 150)
+    }
+
+    @Test
+    fun test_getLocation_returns_null_when_location_manager_dont_have_providers_enabled() {
+        val actualLocation = locationManager._getLocation()
+        assertNull(actualLocation)
+    }
+
+    @Test
+    fun test_getLocation_returns_location_when_location_manager_has_any_providers_enabled() {
+        val systemService = application.getSystemService(Context.LOCATION_SERVICE) as android.location.LocationManager
+        val shadowOf = shadowOf(systemService)
+        shadowOf.setProviderEnabled(android.location.LocationManager.NETWORK_PROVIDER, true)
+        shadowOf.setLastKnownLocation(android.location.LocationManager.NETWORK_PROVIDER, location)
+
+        val actualLocation = locationManager._getLocation()
+
+        assertEquals(location, actualLocation)
+    }
+
+    @Test
+    fun test_getLocation_returns_null_when_location_provider_does_not_have_location() {
+        val systemService = application.getSystemService(Context.LOCATION_SERVICE) as android.location.LocationManager
+        val shadowOf = shadowOf(systemService)
+        shadowOf.setProviderEnabled(android.location.LocationManager.NETWORK_PROVIDER, true)
+        shadowOf.setLastKnownLocation(android.location.LocationManager.NETWORK_PROVIDER, null)
+
+        val actualLocation = locationManager._getLocation()
+
+        assertNull(actualLocation)
+    }
+
+    @Test
+    fun test_getLocation_returns_location_with_best_accuracy() {
+        val systemService = application.getSystemService(Context.LOCATION_SERVICE) as android.location.LocationManager
+        val shadowOf = shadowOf(systemService)
+        shadowOf.setProviderEnabled(android.location.LocationManager.NETWORK_PROVIDER, true)
+        shadowOf.setProviderEnabled(android.location.LocationManager.GPS_PROVIDER, true)
+        shadowOf.setLastKnownLocation(android.location.LocationManager.NETWORK_PROVIDER, location)
+
+        val gpsLocation = Location("").apply {
+            latitude = 17.355
+            longitude = 7.355
+            accuracy = 10f
+        }
+        shadowOf.setLastKnownLocation(android.location.LocationManager.GPS_PROVIDER, gpsLocation)
+
+        val actualLocation = locationManager._getLocation()
+
+        assertEquals(gpsLocation, actualLocation)
     }
 }
