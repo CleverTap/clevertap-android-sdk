feb806d6f9e6d134b78bbe478f98cf624aa674f9
Darshan Pania <darshan@clevertap.com>
2021-04-15 23:13:46 +0530

    feat(SDK-603): Adds pushChargedEvent to CTWebInterface, adds util method to convert JSONArray of JSONObjects to ArrayList of HashMaps for handling charged event items

--- clevertap-core/src/main/java/com/clevertap/android/sdk/CTWebInterface.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/CTWebInterface.java
@@ -2,6 +2,8 @@ package com.clevertap.android.sdk;
 
 import android.webkit.JavascriptInterface;
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.HashMap;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,11 +23,10 @@ public class CTWebInterface {
     /**
      * Method to be called from WebView Javascript to add profile properties in CleverTap
      *
-     * @param key   String value of profile property key
-     * @param value String value of profile property value
+     * @param key   {@link String} value of profile property key
+     * @param value {@link String} value of profile property value
      */
     @JavascriptInterface
-    @SuppressWarnings("unused")
     public void addMultiValueForKey(String key, String value) {
         CleverTapAPI cleverTapAPI = weakReference.get();
         if (cleverTapAPI == null) {
@@ -38,11 +39,10 @@ public class CTWebInterface {
     /**
      * Method to be called from WebView Javascript to add profile properties in CleverTap
      *
-     * @param key    String value of profile property key
-     * @param values Stringified JSON Array of profile property values
+     * @param key    {@link String} value of profile property key
+     * @param values Stringified {@link JSONArray} of profile property values
      */
     @JavascriptInterface
-    @SuppressWarnings("unused")
     public void addMultiValuesForKey(String key, String values) {
         CleverTapAPI cleverTapAPI = weakReference.get();
         if (cleverTapAPI == null) {
@@ -67,12 +67,50 @@ public class CTWebInterface {
     }
 
     /**
-     * Method to be called from WebView Javascript to raise event in CleverTap
+     * Method to be called from WebView Javascript to raise Charged event in CleverTap
      *
-     * @param eventName String value of event name
+     * @param chargeDetails Stringified {@link JSONObject} of charged event details
+     *                      Stringified {@link JSONObject} will be converted to a HashMap<String,Object>
+     * @param items         A {@link JSONArray} which contains up to 15 JSON Object objects,
+     *                      where each JSON Object object describes a particular item purchased
+     *                      {@link JSONArray} of {@link JSONObject}s will be converted to an {@link ArrayList} of
+     *                      {@link HashMap<String,Object>}
      */
     @JavascriptInterface
-    @SuppressWarnings("unused")
+    @SuppressWarnings({"JavaDoc"})
+    public void pushChargedEvent(String chargeDetails, JSONArray items) {
+        CleverTapAPI cleverTapAPI = weakReference.get();
+        if (cleverTapAPI == null) {
+            Logger.d("CleverTap Instance is null.");
+        } else {
+            HashMap<String, Object> chargeDetailsHashMap = new HashMap<>();
+            if (chargeDetails != null) {
+                try {
+                    JSONObject chargeDetailsObject = new JSONObject(chargeDetails);
+                    chargeDetailsHashMap = Utils.convertJSONObjectToHashMap(chargeDetailsObject);
+                } catch (JSONException e) {
+                    Logger.v("Unable to parse eventActions from WebView " + e.getLocalizedMessage());
+                }
+            } else {
+                Logger.v("eventActions passed to CTWebInterface is null");
+                return;
+            }
+            ArrayList<HashMap<String, Object>> itemsArrayList;
+            if (items != null) {
+                itemsArrayList = Utils.convertJSONArrayOfJSONObjectsToArrayListOfHashMaps(items);
+            } else {
+                return;
+            }
+            cleverTapAPI.pushChargedEvent(chargeDetailsHashMap, itemsArrayList);
+        }
+    }
+
+    /**
+     * Method to be called from WebView Javascript to raise event in CleverTap
+     *
+     * @param eventName {@link String} value of event name
+     */
+    @JavascriptInterface
     public void pushEvent(String eventName) {
         CleverTapAPI cleverTapAPI = weakReference.get();
         if (cleverTapAPI == null) {
@@ -85,11 +123,10 @@ public class CTWebInterface {
     /**
      * Method to be called from WebView Javascript to raise event with event properties in CleverTap
      *
-     * @param eventName    String value of event name
-     * @param eventActions Stringified JSON Object of event properties
+     * @param eventName    {@link String} value of event name
+     * @param eventActions Stringified {@link JSONObject} of event properties
      */
     @JavascriptInterface
-    @SuppressWarnings("unused")
     public void pushEvent(String eventName, String eventActions) {
         CleverTapAPI cleverTapAPI = weakReference.get();
         if (cleverTapAPI == null) {
@@ -106,17 +143,14 @@ public class CTWebInterface {
                 Logger.v("eventActions passed to CTWebInterface is null");
             }
         }
-
-
     }
 
     /**
      * Method to be called from WebView Javascript to push profile properties in CleverTap
      *
-     * @param profile Stringified JSON Object of profile properties
+     * @param profile Stringified {@link JSONObject} of profile properties
      */
     @JavascriptInterface
-    @SuppressWarnings("unused")
     public void pushProfile(String profile) {
         CleverTapAPI cleverTapAPI = weakReference.get();
         if (cleverTapAPI == null) {
@@ -138,11 +172,10 @@ public class CTWebInterface {
     /**
      * Method to be called from WebView Javascript to remove profile properties in CleverTap
      *
-     * @param key   String value of profile property key
-     * @param value String value of profile property value
+     * @param key   {@link String} value of profile property key
+     * @param value {@link String} value of profile property value
      */
     @JavascriptInterface
-    @SuppressWarnings("unused")
     public void removeMultiValueForKey(String key, String value) {
         CleverTapAPI cleverTapAPI = weakReference.get();
         if (cleverTapAPI == null) {
@@ -163,11 +196,10 @@ public class CTWebInterface {
     /**
      * Method to be called from WebView Javascript to remove profile properties in CleverTap
      *
-     * @param key    String value of profile property key
-     * @param values Stringified JSON Array of profile property values
+     * @param key    {@link String} value of profile property key
+     * @param values Stringified {@link JSONArray} of profile property values
      */
     @JavascriptInterface
-    @SuppressWarnings("unused")
     public void removeMultiValuesForKey(String key, String values) {
         CleverTapAPI cleverTapAPI = weakReference.get();
         if (cleverTapAPI == null) {
@@ -193,10 +225,9 @@ public class CTWebInterface {
     /**
      * Method to be called from WebView Javascript to remove profile properties for given key in CleverTap
      *
-     * @param key String value of profile property key
+     * @param key {@link String} value of profile property key
      */
     @JavascriptInterface
-    @SuppressWarnings("unused")
     public void removeValueForKey(String key) {
         CleverTapAPI cleverTapAPI = weakReference.get();
         if (cleverTapAPI == null) {
@@ -213,11 +244,10 @@ public class CTWebInterface {
     /**
      * Method to be called from WebView Javascript to set profile properties in CleverTap
      *
-     * @param key    String value of profile property key
-     * @param values Stringified JSON Array of profile property values
+     * @param key    {@link String} value of profile property key
+     * @param values Stringified {@link JSONArray} of profile property values
      */
     @JavascriptInterface
-    @SuppressWarnings("unused")
     public void setMultiValueForKey(String key, String values) {
         CleverTapAPI cleverTapAPI = weakReference.get();
         if (cleverTapAPI == null) {
