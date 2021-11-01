c8ab3fddd115fdbdaf39282c452dd122f887c3dd
Piyush Kukadiya <piyush.kukadiya@clevertap.com>
2021-11-01 14:26:30 +0530

    task(android12_support-sample_app): Fix TLS support for API <= 19 emulators SDK-1114

--- sample/src/main/java/com/clevertap/demo/MyApplication.kt
+++ sample/src/main/java/com/clevertap/demo/MyApplication.kt
@@ -1,13 +1,18 @@
 package com.clevertap.demo
 
 import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.multidex.MultiDex
 import androidx.multidex.MultiDexApplication
 import com.clevertap.android.sdk.ActivityLifecycleCallback
 import com.clevertap.android.sdk.CleverTapAPI
 import com.clevertap.android.sdk.CleverTapAPI.LogLevel.VERBOSE
 import com.clevertap.android.sdk.SyncListener
 import com.clevertap.android.sdk.pushnotification.CTPushNotificationListener
+import com.google.android.gms.security.ProviderInstaller
+import com.google.android.gms.security.ProviderInstaller.ProviderInstallListener
 import org.json.JSONObject
 import java.util.HashMap
 
@@ -18,6 +23,14 @@ class MyApplication : MultiDexApplication(), CTPushNotificationListener {
         CleverTapAPI.setDebugLevel(VERBOSE)
         ActivityLifecycleCallback.register(this)
         super.onCreate()
+
+        ProviderInstaller.installIfNeededAsync(this, object : ProviderInstallListener {
+            override fun onProviderInstalled() {}
+            override fun onProviderInstallFailed(i: Int, intent: Intent?) {
+                Log.i("ProviderInstaller", "Provider install failed ($i) : SSL Problems may occurs")
+            }
+        })
+
         val defaultInstance = CleverTapAPI.getDefaultInstance(this)
         defaultInstance?.syncListener = object : SyncListener {
             override fun profileDataUpdated(updates: JSONObject?) {//no op
@@ -50,5 +63,10 @@ class MyApplication : MultiDexApplication(), CTPushNotificationListener {
 
     override fun onNotificationClickedPayloadReceived(payload: HashMap<String, Any>?) {
         Log.i("MyApplication", "onNotificationClickedPayloadReceived = $payload")
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 }
