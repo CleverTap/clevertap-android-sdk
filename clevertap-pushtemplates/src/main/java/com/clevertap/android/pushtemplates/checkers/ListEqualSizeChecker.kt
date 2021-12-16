44da0ad54662ca739d3cbdcd620c1503d3f0d4f3
william <william@clevertap.com>
2021-12-16 16:18:11 +0530

    task(refactor): Add equal size check for ProductDisplay style

---
+++ clevertap-pushtemplates/src/main/java/com/clevertap/android/pushtemplates/checkers/ListEqualSizeChecker.kt
@@ -0,0 +1,15 @@
+package com.clevertap.android.pushtemplates.checkers
+
+import com.clevertap.android.pushtemplates.PTLog
+
+class ListEqualSizeChecker(val entity: List<Any>?, var size: Int, var errorMsg: String) :
+    SizeChecker<List<Any>>(entity, size, errorMsg) {
+
+    override fun check(): Boolean {
+        val b = entity == null || entity.size != size
+        if (b) {
+            PTLog.verbose("$errorMsg. Not showing notification")
+        }
+        return !b
+    }
+}
--- clevertap-pushtemplates/src/main/java/com/clevertap/android/pushtemplates/validators/ProductDisplayTemplateValidator.kt
+++ clevertap-pushtemplates/src/main/java/com/clevertap/android/pushtemplates/validators/ProductDisplayTemplateValidator.kt
@@ -2,7 +2,8 @@ package com.clevertap.android.pushtemplates.validators
 
 import com.clevertap.android.pushtemplates.checkers.Checker
 
-class ProductDisplayTemplateValidator(private var validator: Validator) : TemplateValidator(validator.keys) {
+class ProductDisplayTemplateValidator(private var validator: Validator) :
+    TemplateValidator(validator.keys) {
 
     override fun validate(): Boolean {
         return validator.validate() && super.validateKeys()// All check must be true
@@ -12,7 +13,7 @@ class ProductDisplayTemplateValidator(private var validator: Validator) : Templa
         return listOf(
             keys[PT_THREE_DEEPLINK_LIST]!!, keys[PT_BIG_TEXT_LIST]!!,
             keys[PT_SMALL_TEXT_LIST]!!, keys[PT_PRODUCT_DISPLAY_ACTION]!!,
-            keys[PT_PRODUCT_DISPLAY_ACTION_CLR]!!, keys[PT_THREE_IMAGE_LIST]!!
+            keys[PT_PRODUCT_DISPLAY_ACTION_CLR]!!, keys[PT_PRODUCT_THREE_IMAGE_LIST]!!
         )
     }
 }
