8f57422c5d3617db060154b1b43f32679fa75847
william <william@clevertap.com>
2021-12-16 12:30:29 +0530

    task(refactor): Update FiveIcon to support 3 or more images/dls

--- clevertap-pushtemplates/src/main/java/com/clevertap/android/pushtemplates/content/FiveIconSmallContentView.kt
+++ clevertap-pushtemplates/src/main/java/com/clevertap/android/pushtemplates/content/FiveIconSmallContentView.kt
@@ -25,6 +25,7 @@ class FiveIconSmallContentView constructor(
         var imageCounter = 0
         for (imageKey in renderer.imageList!!.indices) {
             if (imageKey == 0) {
+                remoteView.setViewVisibility(R.id.cta1, View.VISIBLE)
                 Utils.loadImageURLIntoRemoteView(
                     R.id.cta1,
                     renderer.imageList!![imageKey],
@@ -35,6 +36,7 @@ class FiveIconSmallContentView constructor(
                     imageCounter++
                 }
             } else if (imageKey == 1) {
+                remoteView.setViewVisibility(R.id.cta2, View.VISIBLE)
                 Utils.loadImageURLIntoRemoteView(
                     R.id.cta2,
                     renderer.imageList!![imageKey],
@@ -45,6 +47,7 @@ class FiveIconSmallContentView constructor(
                     remoteView.setViewVisibility(R.id.cta2, View.GONE)
                 }
             } else if (imageKey == 2) {
+                remoteView.setViewVisibility(R.id.cta3, View.VISIBLE)
                 Utils.loadImageURLIntoRemoteView(
                     R.id.cta3,
                     renderer.imageList!![imageKey],
@@ -55,6 +58,7 @@ class FiveIconSmallContentView constructor(
                     remoteView.setViewVisibility(R.id.cta3, View.GONE)
                 }
             } else if (imageKey == 3) {
+                remoteView.setViewVisibility(R.id.cta4, View.VISIBLE)
                 Utils.loadImageURLIntoRemoteView(
                     R.id.cta4,
                     renderer.imageList!![imageKey],
@@ -65,6 +69,7 @@ class FiveIconSmallContentView constructor(
                     remoteView.setViewVisibility(R.id.cta4, View.GONE)
                 }
             } else if (imageKey == 4) {
+                remoteView.setViewVisibility(R.id.cta5, View.VISIBLE)
                 Utils.loadImageURLIntoRemoteView(
                     R.id.cta5,
                     renderer.imageList!![imageKey],
@@ -85,50 +90,56 @@ class FiveIconSmallContentView constructor(
         bundleCTA1.putBoolean("cta1", true)
         bundleCTA1.putString(Constants.DEEP_LINK_KEY, renderer.deepLinkList?.get(0))
         bundleCTA1.putString(Constants.KEY_C2A, PTConstants.PT_5CTA_C2A_KEY + 1 + "_" + renderer.deepLinkList?.get(0))
+        remoteView.setOnClickPendingIntent(
+            R.id.cta1,
+            LaunchPendingIntentFactory.getLaunchPendingIntent(bundleCTA1, context)
+        )
 
         val bundleCTA2 = extras.clone() as Bundle
         bundleCTA2.putBoolean("cta2", true)
         bundleCTA2.putString(Constants.DEEP_LINK_KEY, renderer.deepLinkList?.get(1))
         bundleCTA2.putString(Constants.KEY_C2A, PTConstants.PT_5CTA_C2A_KEY + 2 + "_" + renderer.deepLinkList?.get(1))
-
-        val bundleCTA3 = extras.clone() as Bundle
-        bundleCTA3.putBoolean("cta3", true)
-        bundleCTA3.putString(Constants.DEEP_LINK_KEY, renderer.deepLinkList?.get(2))
-        bundleCTA3.putString(Constants.KEY_C2A, PTConstants.PT_5CTA_C2A_KEY + 3 + "_" + renderer.deepLinkList?.get(2))
-
-        val bundleCTA4 = extras.clone() as Bundle
-        bundleCTA4.putBoolean("cta4", true)
-        bundleCTA4.putString(Constants.DEEP_LINK_KEY, renderer.deepLinkList?.get(3))
-        bundleCTA4.putString(Constants.KEY_C2A, PTConstants.PT_5CTA_C2A_KEY + 4 + "_" + renderer.deepLinkList?.get(3))
-
-        val bundleCTA5 = extras.clone() as Bundle
-        bundleCTA5.putBoolean("cta5", true)
-        bundleCTA5.putString(Constants.DEEP_LINK_KEY, renderer.deepLinkList?.get(4))
-        bundleCTA5.putString(Constants.KEY_C2A, PTConstants.PT_5CTA_C2A_KEY + 5 + "_" + renderer.deepLinkList?.get(4))
-
-        remoteView.setOnClickPendingIntent(
-            R.id.cta1,
-            LaunchPendingIntentFactory.getLaunchPendingIntent(bundleCTA1, context)
-        )
         remoteView.setOnClickPendingIntent(
             R.id.cta2,
             LaunchPendingIntentFactory.getLaunchPendingIntent(bundleCTA2, context)
         )
 
+        val bundleCTA3 = extras.clone() as Bundle
+        bundleCTA3.putBoolean("cta3", true)
+        bundleCTA3.putString(Constants.DEEP_LINK_KEY, renderer.deepLinkList?.get(2))
+        bundleCTA3.putString(Constants.KEY_C2A, PTConstants.PT_5CTA_C2A_KEY + 3 + "_" + renderer.deepLinkList?.get(2))
         remoteView.setOnClickPendingIntent(
             R.id.cta3,
             LaunchPendingIntentFactory.getLaunchPendingIntent(bundleCTA3, context)
         )
 
-        remoteView.setOnClickPendingIntent(
-            R.id.cta4,
-            LaunchPendingIntentFactory.getLaunchPendingIntent(bundleCTA4, context)
-        )
+        if(renderer.deepLinkList != null && renderer.deepLinkList!!.size > 3) {
+            val bundleCTA4 = extras.clone() as Bundle
+            bundleCTA4.putBoolean("cta4", true)
+            bundleCTA4.putString(Constants.DEEP_LINK_KEY, renderer.deepLinkList?.get(3))
+            bundleCTA4.putString(
+                Constants.KEY_C2A,
+                PTConstants.PT_5CTA_C2A_KEY + 4 + "_" + renderer.deepLinkList?.get(3)
+            )
+            remoteView.setOnClickPendingIntent(
+                R.id.cta4,
+                LaunchPendingIntentFactory.getLaunchPendingIntent(bundleCTA4, context)
+            )
+        }
 
-        remoteView.setOnClickPendingIntent(
-            R.id.cta5,
-            LaunchPendingIntentFactory.getLaunchPendingIntent(bundleCTA5, context)
-        )
+        if(renderer.deepLinkList != null && renderer.deepLinkList!!.size > 4) {
+            val bundleCTA5 = extras.clone() as Bundle
+            bundleCTA5.putBoolean("cta5", true)
+            bundleCTA5.putString(Constants.DEEP_LINK_KEY, renderer.deepLinkList?.get(4))
+            bundleCTA5.putString(
+                Constants.KEY_C2A,
+                PTConstants.PT_5CTA_C2A_KEY + 5 + "_" + renderer.deepLinkList?.get(4)
+            )
+            remoteView.setOnClickPendingIntent(
+                R.id.cta5,
+                LaunchPendingIntentFactory.getLaunchPendingIntent(bundleCTA5, context)
+            )
+        }
 
         remoteView.setOnClickPendingIntent(
             R.id.close, PendingIntentFactory.getPendingIntent(
