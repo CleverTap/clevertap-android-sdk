b761997b435fc27618440beec19fef23d22a462b
ansh <ansh@clevertap.com>
2022-04-05 12:38:55 +0530

    [SDK-1496] Test for CTWebInterface.java

--- clevertap-core/src/test/java/com/clevertap/android/sdk/CTWebInterfaceTest.kt
+++ clevertap-core/src/test/java/com/clevertap/android/sdk/CTWebInterfaceTest.kt
@@ -10,7 +10,7 @@ import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
 class CTWebInterfaceTest : BaseTestCase() {
-
+    // CTWebInterface is a wrapper around some functions of the clevertap api
     private lateinit var ctWebInterface: CTWebInterface
     private var ctApi:CleverTapAPI? = null
     override fun setUp() {
@@ -118,22 +118,110 @@ class CTWebInterfaceTest : BaseTestCase() {
     }
 
     @Test
-    fun test_pushProfile_when_ABC_should_XYZ() {
+    fun test_pushProfile_when_CalledWithJsonString_should_CallAssocClevertapApiFunction() {
+        // if profile is null, function returns without any changes
+        ctApi = CleverTapAPI.getDefaultInstance(application)
+        val ctApiSpy = Mockito.spy(ctApi)
+        ctWebInterface = CTWebInterface(ctApiSpy)
+        var profile:String? = null
+        ctWebInterface.pushProfile(profile)
+        Mockito.verify(ctApiSpy, Mockito.never())?.pushProfile(Mockito.anyMap())
+
+        // if profile is not null, function calls associated CT api function
+        profile = "{'key1':'value1'}"
+        ctWebInterface.pushProfile(profile)
+        Mockito.verify(ctApiSpy, Mockito.atMostOnce())?.pushProfile(Utils.convertJSONObjectToHashMap(JSONObject(profile)))
+
+
+
     }
 
     @Test
-    fun test_removeMultiValueForKey_when_ABC_should_XYZ() {
+    fun test_removeMultiValueForKey_when_CalledWithKeyAndValue_should_CallAssocClevertapApiFunction() {
+        // when ctApi is null, calling this function will do nothing.
+        ctApi = null
+        ctWebInterface = CTWebInterface(ctApi)
+        ctWebInterface.removeMultiValueForKey("key", "value")
+
+
+        // when ctApi , key and value are not null, calling this function will call ctApi's internal function
+        ctApi = CleverTapAPI.getDefaultInstance(application)
+        val ctSpy = Mockito.spy(ctApi)
+        ctWebInterface = CTWebInterface(ctSpy)
+
+        ctWebInterface.removeMultiValueForKey(null, "value21")
+        Mockito.verify(ctSpy, Mockito.never())?.removeMultiValueForKey(null, "value21")
+
+        ctWebInterface.removeMultiValueForKey("key21", null)
+        Mockito.verify(ctSpy, Mockito.never())?.removeMultiValueForKey("key21", null)
+
+
+        ctWebInterface.removeMultiValueForKey("key2", "value2")
+        Mockito.verify(ctSpy, Mockito.atLeastOnce())?.removeMultiValueForKey("key2", "value2")
     }
 
     @Test
-    fun test_removeMultiValuesForKey_when_ABC_should_XYZ() {
+    fun test_removeMultiValuesForKey_CalledWithKeyAndValue_should_CallAssocClevertapApiFunction() {
+        // when ctApi is null, calling this function will do nothing.
+        ctApi = null
+        ctWebInterface = CTWebInterface(ctApi)
+        ctWebInterface.removeMultiValuesForKey("key","[{'k1':'v1'},{'k2':'v2'}]")
+
+        // when ctApi is not null, but key(or value) is null,  calling this function will do nothing.
+        ctApi = CleverTapAPI.getDefaultInstance(application)
+        val ctSpy = Mockito.spy(ctApi)
+        ctWebInterface = CTWebInterface(ctSpy)
+        ctWebInterface.removeMultiValuesForKey(null,"[{'k1':'v1'},{'k2':'v2'}]")
+        Mockito.verify(ctSpy,Mockito.never())?.removeMultiValuesForKey(null,Utils.convertJSONArrayToArrayList(JSONArray("[{'k1':'v1'},{'k2':'v2'}]")))
+
+        ctWebInterface.removeMultiValuesForKey("keyx",null)
+        Mockito.verify(ctSpy,Mockito.never())?.removeMultiValuesForKey("keyx",null)
+
+
+        //when ctApi and key and value  are all not null, calling this function will call ctApi's internal function
+        ctWebInterface.removeMultiValuesForKey("key2","[{'k1':'v1'},{'k2':'v2'}]")
+        Mockito.verify(ctSpy,Mockito.atMostOnce())?.removeMultiValuesForKey("key2",Utils.convertJSONArrayToArrayList(JSONArray("[{'k1':'v1'},{'k2':'v2'}]")))
+
+
     }
 
     @Test
-    fun test_removeValueForKey_when_ABC_should_XYZ() {
+    fun test_removeValueForKey_when_CalledWithKey_should_CallAssocClevertapApiFunction() {
+        // if key is null, function returns without any changes
+        ctApi = CleverTapAPI.getDefaultInstance(application)
+        val ctApiSpy = Mockito.spy(ctApi)
+        ctWebInterface = CTWebInterface(ctApiSpy)
+        var key:String? = null
+        ctWebInterface.removeValueForKey(key)
+        Mockito.verify(ctApiSpy, Mockito.never())?.removeValueForKey(key)
+
+        // if profile is not null, function calls associated CT api function
+        key = "key"
+        ctWebInterface.removeValueForKey(key)
+        Mockito.verify(ctApiSpy, Mockito.atMostOnce())?.removeValueForKey(key)
+
     }
 
     @Test
     fun test_setMultiValueForKey_when_ABC_should_XYZ() {
+        // when ctApi is null, calling this function will do nothing.
+        ctApi = null
+        ctWebInterface = CTWebInterface(ctApi)
+        ctWebInterface.setMultiValueForKey("key","[{'k1':'v1'},{'k2':'v2'}]")
+
+        // when ctApi is not null, but key(or value) is null,  calling this function will do nothing.
+        ctApi = CleverTapAPI.getDefaultInstance(application)
+        val ctSpy = Mockito.spy(ctApi)
+        ctWebInterface = CTWebInterface(ctSpy)
+        ctWebInterface.setMultiValueForKey(null,"[{'k1':'v1'},{'k2':'v2'}]")
+        Mockito.verify(ctSpy,Mockito.never())?.setMultiValuesForKey(null,Utils.convertJSONArrayToArrayList(JSONArray("[{'k1':'v1'},{'k2':'v2'}]")))
+
+        ctWebInterface.setMultiValueForKey("keyx",null)
+        Mockito.verify(ctSpy,Mockito.never())?.setMultiValuesForKey("keyx",null)
+
+
+        //when ctApi and key and value  are all not null, calling this function will call ctApi's internal function
+        ctWebInterface.setMultiValueForKey("key2","[{'k1':'v1'},{'k2':'v2'}]")
+        Mockito.verify(ctSpy,Mockito.atMostOnce())?.setMultiValuesForKey("key2",Utils.convertJSONArrayToArrayList(JSONArray("[{'k1':'v1'},{'k2':'v2'}]")))
     }
 }
