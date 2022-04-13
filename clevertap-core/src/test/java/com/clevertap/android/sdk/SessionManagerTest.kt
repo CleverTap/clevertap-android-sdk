dd89776f8232a082c7a5425294c6abde155815d8
ansh <ansh@clevertap.com>
2022-04-13 18:48:23 +0530

    [SDK-1452] changes as per comments part n/n

--- clevertap-core/src/test/java/com/clevertap/android/sdk/SessionManagerTest.kt
+++ clevertap-core/src/test/java/com/clevertap/android/sdk/SessionManagerTest.kt
@@ -1,6 +1,7 @@
 package com.clevertap.android.sdk
 
 
+import android.content.Context
 import com.clevertap.android.sdk.events.EventDetail
 import com.clevertap.android.sdk.validation.Validator
 import com.clevertap.android.shared.test.BaseTestCase
@@ -37,7 +38,6 @@ class SessionManagerTest : BaseTestCase() {
     @Test
     fun test_checkTimeoutSession_when_FunctionIsCalledAndAppLastSeenIsGreaterThan60Mins_should_DestroySession() {
         //1. when appLastSeen is <= 0 , the function returns without any further execution. this implies that destroySession() Was never called
-        //done//TODO@ansh: You can verify by saying destroySession is not called(no interaction)
         var smSpy = Mockito.spy(sessionManagerDef)
         smSpy.appLastSeen = 0
         Mockito.verify(smSpy,Mockito.never()).destroySession()
@@ -47,16 +47,13 @@ class SessionManagerTest : BaseTestCase() {
         smSpy = Mockito.spy(sessionManagerDef)
         smSpy.appLastSeen = System.currentTimeMillis() - (Constants.SESSION_LENGTH_MINS * 60 * 1000 )- 1000
         smSpy.checkTimeoutSession()
-        Mockito.verify(smSpy,Mockito.atMostOnce()).destroySession()
+        Mockito.verify(smSpy,Mockito.times(1)).destroySession()
 
         smSpy = Mockito.spy(sessionManagerDef)
         smSpy.appLastSeen = System.currentTimeMillis()
         smSpy.checkTimeoutSession()
         Mockito.verify(smSpy,Mockito.never()).destroySession()
         assertEquals(CoreMetaData.getCurrentActivity(),null)
-
-        //done//TODO@ansh: Also verify CoreMetaData.setCurrentActivity(null);
-
 
     }
 
@@ -66,12 +63,12 @@ class SessionManagerTest : BaseTestCase() {
         sessionManagerDef = SessionManager(configDef,coreMetaDataSpy,validator,localDataStoreDef)
 
         sessionManagerDef.destroySession()
-        Mockito.verify(coreMetaDataSpy,Mockito.atLeastOnce()).currentSessionId = 0
-        Mockito.verify(coreMetaDataSpy,Mockito.atLeastOnce()).isAppLaunchPushed =false
-        Mockito.verify(coreMetaDataSpy,Mockito.atLeastOnce()).clearSource()
-        Mockito.verify(coreMetaDataSpy,Mockito.atLeastOnce()).clearMedium()
-        Mockito.verify(coreMetaDataSpy,Mockito.atLeastOnce()).clearCampaign()
-        Mockito.verify(coreMetaDataSpy,Mockito.atLeastOnce()).clearWzrkParams()
+        Mockito.verify(coreMetaDataSpy,Mockito.times(1)).currentSessionId = 0
+        Mockito.verify(coreMetaDataSpy,Mockito.times(1)).isAppLaunchPushed =false
+        Mockito.verify(coreMetaDataSpy,Mockito.times(1)).clearSource()
+        Mockito.verify(coreMetaDataSpy,Mockito.times(1)).clearMedium()
+        Mockito.verify(coreMetaDataSpy,Mockito.times(1)).clearCampaign()
+        Mockito.verify(coreMetaDataSpy,Mockito.times(1)).clearWzrkParams()
 
         Mockito.verify(coreMetaDataSpy,Mockito.never()).isFirstSession = false
         Mockito.verify(coreMetaDataSpy,Mockito.never()).isFirstSession = true
@@ -79,18 +76,16 @@ class SessionManagerTest : BaseTestCase() {
         // if coreMetaData has first session set as true, then it will also be switched to false
         coreMetaDataSpy.isFirstSession = true
         sessionManagerDef.destroySession()
-        Mockito.verify(coreMetaDataSpy,Mockito.atLeastOnce()).isFirstSession = false
+        Mockito.verify(coreMetaDataSpy,Mockito.times(1)).isFirstSession = false
 
     }
 
-    //done//TODO@ansh: setter and getter combine to one.
     @Test
     fun test_getAppLastSeenAndSetAppLastSeen_when_FunctionIsCalled_should_ReturnAppLastSeenValue() {
         sessionManagerDef.appLastSeen = 10
         assertEquals(10,sessionManagerDef.appLastSeen)
     }
 
-    //done//TODO@ansh: setter and getter combine to one.
     @Test
     fun test_getLastVisitTimeAndSetLastVisitTime_when_FunctionIsCalled_should_SetTimeOfLastAppLaunchEventFireInLocalDataStore() {
         val localDataStoreMockk = Mockito.mock(LocalDataStore::class.java)
@@ -113,7 +108,7 @@ class SessionManagerTest : BaseTestCase() {
     fun test_lazyCreateSession_when_FunctionIsCalledWithContext_should_CreateSessionIfApplicable() {
         val coreMetaDataSpy = Mockito.spy(coreMetaData)
         sessionManagerDef = SessionManager(configDef,coreMetaDataSpy,validator,localDataStoreDef)
-        val ctxSpy = Mockito.spy(application)
+        var ctxSpy = Mockito.spy(application)
 
         // when current session is going on (i.e when currentSessionId>0 ) session is not created . we verify by verifying coreMetaDataSpy call
         coreMetaDataSpy.currentSessionId = 4
@@ -122,14 +117,43 @@ class SessionManagerTest : BaseTestCase() {
 
         // when current session has ended (i.e when currentSessionId<=0 ) session is  created .
         // we verify by verifying coreMetaDataSpy calls
+        ctxSpy = Mockito.spy(application)
         coreMetaDataSpy.currentSessionId = 0
         sessionManagerDef.lazyCreateSession(ctxSpy)
-        Mockito.verify(coreMetaDataSpy,Mockito.atMostOnce()).isFirstRequestInSession = true
-        Mockito.verify(coreMetaDataSpy,Mockito.atLeastOnce()).currentSessionId = Mockito.anyInt()
-        //todo@piyush : how?help needed //TODO@ansh: Also verify that createSession() is called.
+        Mockito.verify(coreMetaDataSpy,Mockito.times(1)).isFirstRequestInSession = true
 
     }
 
-    ////TODO@ansh: createSession() unit test is missing
+    @Test
+    fun testCreateSession() {
+        // when current session has ended (i.e when currentSessionId<=0 ) session is  created .
+        // we verify by verifying coreMetaDataSpy calls
+        var coreMetaDataSpy = Mockito.spy(coreMetaData).also { it.currentSessionId = 0 }
+        var ctxSpy = Mockito.spy(application)
+        sessionManagerDef = SessionManager(configDef,coreMetaDataSpy,validator,localDataStoreDef)
+
+        // when lazyCreateSession is called while coreMetaData.currentSessionId ==0 , createSession gets called and
+        // 1. coreMetaData.currentSessionId to System.currentTimeMillis() / 1000
+        // 2. value of lastSessionId in cache is set to value of coreMetaData.getCurrentSessionId()
+
+        sessionManagerDef.lazyCreateSession(ctxSpy)
+        val expectedSettedValue = (System.currentTimeMillis() / 1000).toInt()
+        Thread.sleep(1000)
+        Mockito.verify(coreMetaDataSpy,Mockito.times(1)).currentSessionId = 0//(System.currentTimeMillis() / 1000).toInt()
+        val settedValue = ctxSpy.getSharedPreferences("WizRocket",Context.MODE_PRIVATE).getInt("lastSessionId:id",-1)
+        assertEquals(expectedSettedValue,settedValue)
+
+        // when lastSessionTime <= 0 ,xyz . // when lastSessionID ! = 0
+        coreMetaDataSpy = Mockito.spy(coreMetaData).also { it.currentSessionId = 0 }
+        ctxSpy = Mockito.spy(application)
+        sessionManagerDef = SessionManager(configDef, coreMetaDataSpy, validator, localDataStoreDef)
+        ctxSpy.getSharedPreferences("WizRocket", Context.MODE_PRIVATE).edit().putInt("lastSessionId:id", 0).putInt("sexe:id", 10)/*.putInt("lastSessionId",11).putInt("sexe",13)*/.commit()
+
+        sessionManagerDef.lazyCreateSession(ctxSpy)
+        Mockito.verify(coreMetaDataSpy,Mockito.times(1)).lastSessionLength = 10
+        Mockito.verify(coreMetaDataSpy,Mockito.times(1)).isFirstSession= true
+
+    }
+
 
 }
