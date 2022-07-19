ddafe1fc49d01c57e97bce69058236377a52c032
ansh <ansh@clevertap.com>
2022-07-19 13:46:36 +0530

    [SDK-1713] updated tests

--- clevertap-core/src/main/java/com/clevertap/android/sdk/db/DBAdapter.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/db/DBAdapter.java
@@ -19,7 +19,6 @@ import com.clevertap.android.sdk.inbox.CTMessageDAO;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import org.jetbrains.annotations.TestOnly;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -844,7 +843,6 @@ public class DBAdapter {
         return dbHelper.belowMemThreshold();
     }
 
-    @TestOnly
     void cleanInternal(Table table, long expiration) {
 
         final long time = (System.currentTimeMillis() - expiration) / 1000;
