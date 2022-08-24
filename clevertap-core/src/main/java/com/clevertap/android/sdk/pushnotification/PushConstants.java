22e3689f48c5e5863dcfe96f3af0cf4073d52274
ansh <ansh@clevertap.com>
2022-08-24 15:19:16 +0530

    [SDK-1968] fix server region setting logic

--- clevertap-core/src/main/java/com/clevertap/android/sdk/pushnotification/PushConstants.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/pushnotification/PushConstants.java
@@ -105,7 +105,7 @@ public interface PushConstants {
         public void  setServerRegion(@Nullable String region){
             android.util.Log.e("CleverTap_XPS_150","PushConstants: setServerRegion called with region:"+region);
 
-            this.serverRegion = (region==null) ? "" : null;
+            this.serverRegion = (region==null) ? "" : region;
         }
 
         public String getServerRegion(){
