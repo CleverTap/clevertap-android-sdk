63f49fa51c75941b3a963908764c3f7ce863dfeb
Shivam Sharma <shivam.sharma@clevertap.com>
2023-03-20 09:51:19 +0530

    task(SDK-2811): add support for buttonIndex in onInboxItemClicked callback

--- clevertap-core/src/main/java/com/clevertap/android/sdk/InboxMessageListener.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/InboxMessageListener.java
@@ -4,7 +4,14 @@ import com.clevertap.android.sdk.inbox.CTInboxMessage;
 
 public interface InboxMessageListener {
     /**
-     * callback to transfer payload when an inbox item is clicked
+     * This callback notifies about the following:
+     * - App Inbox item click
+     * - CTA clicks for which no custom key-value pairs are associated.
+     *
+     * @param message     - the instance of {@link CTInboxMessage}
+     * @param buttonIndex - the button index corresponds to the CTA button clicked (0, 1, or 2) in
+     *                      the App Inbox, which supports up to three CTAs.
+     *                      A value of -1 indicates an app inbox item click.
      */
-    void onInboxItemClicked(CTInboxMessage message);
+    void onInboxItemClicked(CTInboxMessage message, int buttonIndex);
 }
