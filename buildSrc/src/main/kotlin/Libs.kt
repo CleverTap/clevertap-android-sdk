6b30245eef57f288432707096b8e15ab1bbc42dd
william <william@clevertap.com>
2023-05-26 00:28:34 +0530

    task(SDK-2955) - Refactor libraryVersion keys, update Libs.kt

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -11,7 +11,18 @@ object Android {
     const val clevertapGeofenceSdk="1.2.0"
     const val clevertapHmsSdk="1.3.2"
     const val clevertapXiaomiSdk="1.5.2"
-    const val clevertapPushtemplates="1.0.9"
+    const val clevertapPushTemplates ="1.0.9"
+    const val clevertapBoM ="1.0.0"
+
+    const val clevertap_android_sdk: String = "com.clevertap.android:clevertap-android-sdk:_"
+
+    const val clevertap_geofence_sdk: String = "com.clevertap.android:clevertap-geofence-sdk:_"
+
+    const val clevertap_hms_sdk: String = "com.clevertap.android:clevertap-hms-sdk:_"
+
+    const val clevertap_xiaomi_sdk: String = "com.clevertap.android:clevertap-xiaomi-sdk:_"
+
+    const val clevertap_push_templates_sdk: String = "com.clevertap.android:push-templates:_"
 }
 
 object SDKTest {
