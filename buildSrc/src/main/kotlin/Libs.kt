023d115116fee10b4721f9cd8c2713c79887b3f4
william <william@clevertap.com>
2023-05-30 19:59:50 +0530

    task(SDK-2955) - Refactor versions.properties to add only versions needed for `copyTemplates` task

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,28 +1,47 @@
-object Android {
+object Libs {
+
+    const val appcompat: String = "androidx.appcompat:appcompat:_"
+    const val concurrent_futures: String = "androidx.concurrent:concurrent-futures:_"
+    const val androidx_core_core: String = "androidx.core:core:_"
+    const val recyclerview: String = "androidx.recyclerview:recyclerview:_"
+    const val viewpager: String = "androidx.viewpager:viewpager:_"
+    const val work_runtime: String = "androidx.work:work-runtime:_"
+    const val installreferrer: String = "com.android.installreferrer:installreferrer:_"
+    const val com_android_tools_build_gradle: String = "com.android.tools.build:gradle:_"
+    const val clevertap_android_sdk: String = "com.clevertap.android:clevertap-android-sdk:_"
+    const val clevertap_rendermax_sdk: String = "com.clevertap.android:clevertap-rendermax-sdk:_"
+    const val clevertap_geofence_sdk: String = "com.clevertap.android:clevertap-geofence-sdk:_"
+    const val clevertap_hms_sdk: String = "com.clevertap.android:clevertap-hms-sdk:_"
+    const val clevertap_xiaomi_sdk: String = "com.clevertap.android:clevertap-xiaomi-sdk:_"
+    const val clevertap_push_templates_sdk: String = "com.clevertap.android:push-templates:_"
+    const val glide: String = "com.github.bumptech.glide:glide:_"
+    const val exoplayer: String = "com.google.android.exoplayer:exoplayer:_"
+    const val exoplayer_hls: String = "com.google.android.exoplayer:exoplayer-hls:_"
+    const val exoplayer_ui: String = "com.google.android.exoplayer:exoplayer-ui:_"
+    const val play_services_ads: String = "com.google.android.gms:play-services-ads:_"
+    const val play_services_location: String = "com.google.android.gms:play-services-location:_"
+    const val material: String = "com.google.android.material:material:_"
+    const val firebase_messaging: String = "com.google.firebase:firebase-messaging:_"
+    const val google_services: String = "com.google.gms:google-services:_"
+    const val agcp: String = "com.huawei.agconnect:agcp:_"
+    const val push: String = "com.huawei.hms:push:_"
+}
+object AndroidVersion {
 
     // Android SDK
-    const val compileSdkVersionVal = 33
-    const val targetSdkVersionVal = 33
-    const val buildToolsVersionVal = "33.0.0"
-    const val minSdkVersionVal = 19
+    const val compileSdkVersion = 33
+    const val targetSdkVersion = 33
+    const val buildToolsVersion = "33.0.0"
+    const val minSdkVersion = 19
 
-    const val clevertapAndroidSdk="5.0.0"
-    const val clevertapRendermaxSdk="1.0.3"
-    const val clevertapGeofenceSdk="1.2.0"
-    const val clevertapHmsSdk="1.3.2"
-    const val clevertapXiaomiSdk="1.5.2"
-    const val clevertapPushTemplates ="1.0.9"
-    const val clevertapBoM ="1.0.0"
+    const val clevertapAndroidSdk = "5.0.0"
+    const val clevertapRendermaxSdk = "1.0.3"
+    const val clevertapGeofenceSdk = "1.2.0"
+    const val clevertapHmsSdk = "1.3.2"
+    const val clevertapXiaomiSdk = "1.5.2"
+    const val clevertapPushTemplates = "1.0.9"
+    const val clevertapBoM = "1.0.0"
 
-    const val clevertap_android_sdk: String = "com.clevertap.android:clevertap-android-sdk:_"
-
-    const val clevertap_geofence_sdk: String = "com.clevertap.android:clevertap-geofence-sdk:_"
-
-    const val clevertap_hms_sdk: String = "com.clevertap.android:clevertap-hms-sdk:_"
-
-    const val clevertap_xiaomi_sdk: String = "com.clevertap.android:clevertap-xiaomi-sdk:_"
-
-    const val clevertap_push_templates_sdk: String = "com.clevertap.android:push-templates:_"
 }
 
 object SDKTest {
