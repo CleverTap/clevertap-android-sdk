bdb1a5037ce17647791311076fa1f380c69d011d
piyush-kukadiya <piyush.kukadiya@clevertap.com>
2023-06-05 17:34:02 +0530

    feat(improve_pn_rendering) - add documentation in DownloadedBitmap SDK-2938

--- clevertap-core/src/main/java/com/clevertap/android/sdk/network/DownloadedBitmap.kt
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/network/DownloadedBitmap.kt
@@ -2,11 +2,24 @@ package com.clevertap.android.sdk.network
 
 import android.graphics.Bitmap
 
+/**
+ * Represents a downloaded bitmap with its associated status and download time.
+ *
+ * @property bitmap The downloaded bitmap. Can be null if the download was unsuccessful.
+ * @property status The status of the downloaded bitmap.
+ * @property downloadTime The time taken to download the bitmap, in milliseconds.
+ */
 data class DownloadedBitmap(
     var bitmap: Bitmap?,
     var status: Status,
     var downloadTime: Long
 ) {
+
+    /**
+     * Enum class representing the status of a downloaded bitmap.
+     *
+     * @property statusValue The value associated with the status.
+     */
     enum class Status(val statusValue: String) {
         NO_IMAGE("NO_IMAGE"),
         SUCCESS("SUCCESS"),
