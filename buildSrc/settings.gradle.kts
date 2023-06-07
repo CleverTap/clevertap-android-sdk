e7bf09594157297b1f981a4b030f2839683ed3e4
william <william@clevertap.com>
2023-06-07 18:18:20 +0530

    task(SDK-2955) - bump AGP to v7.4.2, gradle-wrapper.properties to support version catalog, refactor settings.gradle.kts to support version catalog dependencies

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,25 +1,14 @@
-pluginManagement {
-    plugins {
-        id("de.fayard.refreshVersions") version "0.10.0"
-////                                # available:"0.10.1"
-////                                # available:"0.11.0"
-////                                # available:"0.20.0"
-////                                # available:"0.21.0"
-////                                # available:"0.22.0"
-////                                # available:"0.23.0"
-////                                # available:"0.30.0"
-////                                # available:"0.30.1"
-////                                # available:"0.30.2"
-////                                # available:"0.40.0"
-////                                # available:"0.40.1"
-////                                # available:"0.40.2"
-////                                # available:"0.50.0"
-////                                # available:"0.50.1"
-////                                # available:"0.50.2"
+@file:Suppress("UnstableApiUsage")
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
     }
 }
-
-plugins {
-    id("de.fayard.refreshVersions")
-}
-
