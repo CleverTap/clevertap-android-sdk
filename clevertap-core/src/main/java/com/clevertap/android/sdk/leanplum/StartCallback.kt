8af47e97be715799dad8aa649292ac587d757183
Hristo Borisov <hristo.borisov@leanplum.com>
2023-06-13 20:14:16 +0300

    [SDK-2728] Remove start callback

--- clevertap-core/src/main/java/com/clevertap/android/sdk/leanplum/StartCallback.kt
+++
@@ -1,6 +0,0 @@
-package com.clevertap.android.sdk.leanplum
-
-// TODO Think about replacing this one with callback after App Launched is triggered
-interface StartCallback {
-  fun onResponse(success: Boolean)
-}
