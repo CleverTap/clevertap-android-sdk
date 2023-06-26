e2076bd53bba96806b44ce39313743faae2b4ea5
piyush-kukadiya <piyush.kukadiya@clevertap.com>
2023-06-26 16:35:39 +0530

    feat(improve_pn_rendering) - refactor bitmap utility methods to classes SDK-2938

---
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/bitmap/NotificationBitmapDownloadRequestHandler.kt
@@ -0,0 +1,38 @@
+package com.clevertap.android.sdk.bitmap
+
+import com.clevertap.android.sdk.Constants
+import com.clevertap.android.sdk.Logger
+import com.clevertap.android.sdk.Utils
+import com.clevertap.android.sdk.network.DownloadedBitmap
+import com.clevertap.android.sdk.network.DownloadedBitmap.Status.NO_IMAGE
+import com.clevertap.android.sdk.network.DownloadedBitmapFactory
+
+class NotificationBitmapDownloadRequestHandler(
+    private val iBitmapDownloadRequestHandler: IBitmapDownloadRequestHandler
+) : IBitmapDownloadRequestHandler {
+
+    override fun handleRequest(bitmapDownloadRequest: BitmapDownloadRequest): DownloadedBitmap {
+        Logger.v("handling bitmap download request in NotificationBitmapDownloadRequestHandler....")
+
+        val (srcUrl, fallbackToAppIcon, context) = bitmapDownloadRequest
+
+        // If the bitmap path is not specified
+
+        if (srcUrl.isNullOrBlank()) {
+            return Utils.getDownloadedBitmapPostFallbackIconCheck(
+                fallbackToAppIcon, context,
+                DownloadedBitmapFactory.nullBitmapWithStatus(NO_IMAGE)
+            )
+        }
+
+        // Simply stream the bitmap
+        if (!srcUrl.startsWith("http")) {
+            bitmapDownloadRequest.bitmapPath = "${Constants.ICON_BASE_URL}/$srcUrl"
+        }
+
+
+        val downloadedBitmap: DownloadedBitmap = iBitmapDownloadRequestHandler.handleRequest(bitmapDownloadRequest)
+
+        return Utils.getDownloadedBitmapPostFallbackIconCheck(fallbackToAppIcon, context, downloadedBitmap)
+    }
+}
