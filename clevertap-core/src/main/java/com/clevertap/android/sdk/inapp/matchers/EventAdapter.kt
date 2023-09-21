f1679a60ad99eebc2ca9a7350070eede75bc7166
piyush.kukadiya <piyush.kukadiya@clevertap.com>
2023-09-21 14:55:19 +0530

    feat(inapp) - add EventAdapter implementation SDK-3285

--- clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/matchers/EventAdapter.kt
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/matchers/EventAdapter.kt
@@ -1,29 +1,35 @@
 package com.clevertap.android.sdk.inapp.matchers
 
+/**
+ * Represents an event and its associated properties.
+ *
+ * @property eventName The name of the event.
+ * @property eventProperties A map of event properties, where keys are property names and values are property values.
+ * @property items A list of items associated with the event (used for charged events). Defaults to an empty list.
+ */
 class EventAdapter(
-  private val eventName: String,
-  private val eventProperties: Map<String, Any>,
-  private val items: List<Map<String, Any>> = listOf(), // for chargedEvent only
+    val eventName: String,
+    val eventProperties: Map<String, Any>,
+    val items: List<Map<String, Any>> = listOf(), // for chargedEvent only
 ) {
 
-  fun getEventName(): String {
-    return eventName
-  }
+    /**
+     * Gets the property value for the specified property name.
+     *
+     * @param propertyName The name of the property to retrieve.
+     * @return A [TriggerValue] representing the property value, or `null` if the property is not present.
+     */
+    fun getPropertyValue(propertyName: String): TriggerValue? =
+        eventProperties[propertyName]?.let { TriggerValue(it) }
 
-  fun getPropertyValue(propertyName: String): TriggerValue? {
-    // TODO return null when property is missing
-    return TriggerValue()
-  }
-
-  fun getItemValue(propertyName: String): TriggerValue? {
-    val itemValues = mutableListOf<Any>()
-    items.forEach {
-      val value = it[propertyName]
-      if (value != null) {
-        itemValues.add(value)
-      }
+    /**
+     * Gets the item value for the specified property name from the list of items.
+     *
+     * @param propertyName The name of the property to retrieve from the items.
+     * @return A [TriggerValue] representing the item value, or `null` if no items have the property.
+     */
+    fun getItemValue(propertyName: String): TriggerValue? {
+        val itemValues = items.mapNotNull { it[propertyName] }
+        return if (itemValues.isEmpty()) null else TriggerValue(itemValues)
     }
-    return if (itemValues.isEmpty()) null else TriggerValue(itemValues)
-  }
-
 }
