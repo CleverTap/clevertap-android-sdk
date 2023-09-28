5b9cf45e5d29ae55ea57f876d66a09c4ea2d7ed4
Shivam Sharma <shivam.sharma@clevertap.com>
2023-09-28 16:03:32 +0530

    feat(inapp) - add ImpressionManager implementation with unit tests SDK-3288

--- clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/ImpressionManager.kt
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/ImpressionManager.kt
@@ -2,105 +2,133 @@ package com.clevertap.android.sdk.inapp
 
 import com.clevertap.android.sdk.utils.Clock
 import java.util.Calendar
+import java.util.Date
 import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 class ImpressionManager(
-  private val impressionStore: ImpressionStore,
-  private val clock: Clock = Clock.SYSTEM,
-  private val locale: Locale = Locale.US,
+    private val impressionStore: ImpressionStore,
+    private val clock: Clock = Clock.SYSTEM,
+    private val locale: Locale = Locale.US,
 ) {
 
-  // TODO add the offset to shorten the timestamps
-//  private val DATE_OFFSET = ..
+    // TODO add the offset to shorten the timestamps
+    // private val DATE_OFFSET = ..
 
-  private val sessionImpressions: MutableMap<String, MutableList<Long>> = mutableMapOf()
-  private var sessionImpressionsTotal = 0
+    private var sessionImpressions: MutableMap<String, MutableList<Long>> = mutableMapOf()
+    private var sessionImpressionsTotal = 0
 
-  fun recordImpression(campaignId: String) {
-    sessionImpressionsTotal++
-    val now = clock.currentTimeSeconds()
-    val records = sessionImpressions.getOrPut(campaignId) { mutableListOf() }
-    records.add(now)
-
-    impressionStore.write(campaignId, now)
-  }
-
-  fun perSession(campaignId: String): Int {
-    return sessionImpressions[campaignId]?.size ?: 0
-  }
-
-  fun perSessionTotal(): Int {
-    return sessionImpressionsTotal
-  }
-
-  fun perSecond(campaignId: String, seconds: Int): Int {
-    val now = clock.currentTimeSeconds()
-    return getImpressionCount(campaignId, now - seconds)
-  }
-
-  fun perMinute(campaignId: String, minutes: Int): Int {
-    val now = clock.currentTimeSeconds()
-    val offset = TimeUnit.MINUTES.toSeconds(minutes.toLong())
-    return getImpressionCount(campaignId, now - offset)
-  }
-
-  fun perHour(campaignId: String, hours: Int): Int {
-    val now = clock.currentTimeSeconds()
-    val offset = TimeUnit.HOURS.toSeconds(hours.toLong())
-    return getImpressionCount(campaignId, now - offset)
-  }
-
-  fun perDay(campaignId: String, days: Int): Int { // TODO maybe the perDay could be changed to use the Calendar functionality
-    val now = clock.currentTimeSeconds()
-    val offset = TimeUnit.DAYS.toSeconds(days.toLong())
-    return getImpressionCount(campaignId, now - offset)
-  }
-
-  fun perWeek(campaignId: String, weeks: Int): Int { // start of week is Monday for some countries and Sunday in others
-    // TODO create calendar and reset time and move day to the Monday or Sunday
-    val calendar = Calendar.getInstance(locale).apply { // TODO reuse instance and just set time?
-      timeInMillis = clock.currentTimeMillis()
-      set(Calendar.HOUR_OF_DAY, 0)
-      set(Calendar.MINUTE, 0)
-      set(Calendar.SECOND, 0)
-      set(Calendar.MILLISECOND, 0)
+    fun setSessionImpressions(sessionImpressions: MutableMap<String, MutableList<Long>>) {
+        this.sessionImpressions.clear()
+        this.sessionImpressions.putAll(sessionImpressions)
     }
 
-    // move back number of days till start of week
-    val daysFromStartOfWeek = calendar.get(Calendar.DAY_OF_WEEK) - calendar.firstDayOfWeek
-    calendar.add(Calendar.DAY_OF_MONTH, -daysFromStartOfWeek)
-    // TODO check if dayOfMonth is getting negative - almost certainly the library is taking care of it
+    fun recordImpression(campaignId: String) {
+        sessionImpressionsTotal++
+        val now = clock.currentTimeSeconds()
+        val records = sessionImpressions.getOrPut(campaignId) { mutableListOf() }
+        records.add(now)
 
-    // move back number of weeks
-    if (weeks > 1) {
-      calendar.add(Calendar.WEEK_OF_YEAR, -(weeks-1))
+        impressionStore.write(campaignId, now)
     }
 
-    val startingDayTimestamp = TimeUnit.MILLISECONDS.toSeconds(calendar.timeInMillis)
-    return getImpressionCount(campaignId, startingDayTimestamp)
-  }
-
-  fun getImpressionCount(campaignId: String): Int {
-    return impressionStore.read(campaignId).size
-  }
-
-  fun getImpressionCount(campaignId: String, timestampStart: Long): Int {
-    val timestamps = impressionStore.read(campaignId)
-
-    var count = 0
-    for (i in (0..timestamps.lastIndex).reversed()) {
-      if (timestampStart > timestamps[i]) {
-        break
-      }
-      count++
+    fun perSession(campaignId: String): Int {
+        return sessionImpressions[campaignId]?.size ?: 0
     }
-    return count
-  }
 
-  fun clearSessionData() {
-    sessionImpressions.clear()
-    sessionImpressionsTotal = 0
-  }
+    fun perSessionTotal(): Int {
+        return sessionImpressionsTotal
+    }
 
+    fun perSecond(campaignId: String, seconds: Int): Int {
+        val now = clock.currentTimeSeconds()
+        return getImpressionCount(campaignId, now - seconds)
+    }
+
+    fun perMinute(campaignId: String, minutes: Int): Int {
+        val now = clock.currentTimeSeconds()
+        val offset = TimeUnit.MINUTES.toSeconds(minutes.toLong())
+        return getImpressionCount(campaignId, now - offset)
+    }
+
+    fun perHour(campaignId: String, hours: Int): Int {
+        val now = clock.currentTimeSeconds()
+        val offset = TimeUnit.HOURS.toSeconds(hours.toLong())
+        return getImpressionCount(campaignId, now - offset)
+    }
+
+    fun perDay(campaignId: String, days: Int): Int {
+        val calendar = Calendar.getInstance(locale).apply { // TODO reuse instance and just set time?
+            val currentDate = Date()
+            // Set the calendar's time to the current date and time
+            time = currentDate
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+
+        // Subtract the specified number of days from the current date
+        calendar.add(Calendar.DAY_OF_YEAR, -days)
+
+        // Get the resulting date, which represents the start date for the calculation
+        val startOfWeek = calendar.time
+        val startingDayTimestamp = TimeUnit.MILLISECONDS.toSeconds(startOfWeek.time)
+
+        return getImpressionCount(campaignId, startingDayTimestamp)
+    }
+
+    fun perWeek(
+        campaignId: String, weeks: Int
+    ): Int {
+        // start of week is Monday for some countries and Sunday in others
+        val calendar = Calendar.getInstance(locale).apply { // TODO reuse instance and just set time?
+            val currentDate = Date()
+            // Set the calendar's time to the current date and time
+            time = currentDate
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+
+        // Get the first weekday based on the user's locale
+        val firstWeekday = calendar.firstDayOfWeek
+
+        // Calculate the number of days to subtract to reach the starting day of the week
+        val daysToSubtract = (calendar.get(Calendar.DAY_OF_WEEK) - firstWeekday + 7) % 7
+
+        // Move back number of days till start of week
+        calendar.add(Calendar.DAY_OF_YEAR, -daysToSubtract)
+
+        // Move back the number of weeks
+        if (weeks > 1) {
+            calendar.add(Calendar.WEEK_OF_YEAR, -weeks)
+        }
+
+        val startingDayTimestamp = TimeUnit.MILLISECONDS.toSeconds(calendar.timeInMillis)
+        return getImpressionCount(campaignId, startingDayTimestamp)
+    }
+
+    fun getImpressionCount(campaignId: String): Int {
+        return impressionStore.read(campaignId).size
+    }
+
+    private fun getImpressionCount(campaignId: String, timestampStart: Long): Int {
+        val timestamps = impressionStore.read(campaignId)
+
+        var count = 0
+        for (i in (0..timestamps.lastIndex).reversed()) {
+            if (timestampStart > timestamps[i]) {
+                break
+            }
+            count++
+        }
+        return count
+    }
+
+    fun clearSessionData() {
+        sessionImpressions.clear()
+        sessionImpressionsTotal = 0
+    }
 }
---
+++ clevertap-core/src/test/java/com/clevertap/android/sdk/inapp/ImpressionManagerTest.kt
@@ -0,0 +1,300 @@
+package com.clevertap.android.sdk.inapp
+
+import com.clevertap.android.sdk.utils.Clock
+import com.clevertap.android.shared.test.BaseTestCase
+import org.junit.Test
+import org.mockito.*
+import org.mockito.Mockito.*
+import java.util.Calendar
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+import kotlin.test.assertEquals
+
+class ImpressionManagerTest : BaseTestCase() {
+    @Mock
+    private lateinit var clock: Clock
+
+    private lateinit var impressionManager: ImpressionManager
+
+    override fun setUp() {
+        super.setUp()
+        MockitoAnnotations.openMocks(this)
+
+        impressionManager = ImpressionManager(
+            impressionStore = ImpressionStore(application, "testDeviceId"),
+            clock = clock,
+            locale = Locale.US
+        );
+    }
+
+    @Test
+    fun testRecordImpression() {
+        // Arrange
+        val campaignId = "campaign123"
+        val currentTime = 123456L // Replace with a desired timestamp
+
+        // Mock the clock to return a fixed timestamp
+        `when`(clock.currentTimeSeconds()).thenReturn(currentTime)
+
+        // Act
+        impressionManager.recordImpression(campaignId)
+
+        // Assert
+        assertEquals(1, impressionManager.perSessionTotal()) // Expecting one impression recorded
+    }
+
+    @Test
+    fun testPerSessionWithCampaignIdPresent() {
+        // Arrange
+        val campaignId = "campaign123"
+
+        // Create a sessionImpressions map with a campaignId and associated list
+        val sessionImpressions = mutableMapOf(
+            campaignId to mutableListOf(1L, 2L, 3L)
+        )
+
+        // Set the sessionImpressions map in the impressionManager
+        impressionManager.setSessionImpressions(sessionImpressions)
+
+        // Act
+        val result = impressionManager.perSession(campaignId)
+
+        // Assert
+        assertEquals(3, result)
+    }
+
+    @Test
+    fun testPerSessionWithCampaignIdMissing() {
+        // Arrange
+        val campaignId = "campaign123"
+
+        // Act
+        val result = impressionManager.perSession(campaignId)
+
+        // Assert
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun testPerSessionTotalWithInitialValue() {
+        // Act
+        val result = impressionManager.perSessionTotal()
+
+        // Assert
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun testPerSessionTotalAfterRecordImpressions() {
+        // Arrange
+        val campaignId = "campaign123"
+
+        // Record some impressions to update the perSessionTotal value
+        impressionManager.recordImpression(campaignId)
+        impressionManager.recordImpression(campaignId)
+        impressionManager.recordImpression(campaignId)
+
+        // Act
+        val result = impressionManager.perSessionTotal()
+
+        // Assert
+        assertEquals(3, result) // Expecting the total number of impressions recorded
+    }
+
+    @Test
+    fun testPerSecondWithImpressions() {
+        // Arrange
+        val campaignId = "campaign123"
+        val currentTimeSeconds = 1000L
+        val seconds = 10
+
+        `when`(clock.currentTimeSeconds()).thenReturn(currentTimeSeconds)
+
+        // Record one impression
+        impressionManager.recordImpression(campaignId)
+
+        // Act
+        val result = impressionManager.perSecond(campaignId, seconds)
+
+        // Assert
+        assertEquals(1, result) // Expecting 1 impression within the last 10 seconds
+    }
+
+    @Test
+    fun testPerSecondWithoutImpressions() {
+        // Arrange
+        val campaignId = "campaign123"
+        val currentTimeSeconds = 1000L
+        val seconds = 10
+
+        `when`(clock.currentTimeSeconds()).thenReturn(currentTimeSeconds)
+
+        // No impressions recorded, so the result should be 0
+
+        // Act
+        val result = impressionManager.perSecond(campaignId, seconds)
+
+        // Assert
+        assertEquals(0, result) // Expecting 0 impressions within the last 10 seconds
+    }
+
+    @Test
+    fun testPerMinuteWithImpressions() {
+        // Arrange
+        val campaignId = "campaign123"
+        val currentTimestamp = 1000L // Replace with a desired timestamp
+        val minutesOffset = 5 // Minutes to subtract
+
+        `when`(clock.currentTimeSeconds()).thenReturn(currentTimestamp)
+
+        // Act
+        impressionManager.recordImpression(campaignId) // Record an impression
+        val result = impressionManager.perMinute(campaignId, minutesOffset)
+
+        // Assert
+        assertEquals(1, result) // Expecting 1 impression recorded within the last 5 minutes
+    }
+
+    @Test
+    fun testPerMinuteWithoutImpressions() {
+        // Arrange
+        val campaignId = "campaign123"
+        val currentTimestamp = 1000L // Replace with a desired timestamp
+        val minutesOffset = 5 // Minutes to subtract
+
+        `when`(clock.currentTimeSeconds()).thenReturn(currentTimestamp)
+
+        // No impressions recorded, so the result should be 0
+
+        // Act
+        val result = impressionManager.perMinute(campaignId, minutesOffset)
+
+        // Assert
+        assertEquals(0, result) // Expecting 0 impressions within the last 5 minutes
+    }
+
+    @Test
+    fun testPerHourWithImpressions() {
+        // Arrange
+        val campaignId = "campaign123"
+        val currentTimestamp = 1000L // Replace with a desired timestamp
+        val hoursOffset = 2 // Hours to subtract
+
+        `when`(clock.currentTimeSeconds()).thenReturn(currentTimestamp)
+
+        // Act
+        impressionManager.recordImpression(campaignId) // Record an impression
+        val result = impressionManager.perHour(campaignId, hoursOffset)
+
+        // Assert
+        assertEquals(1, result) // Expecting 1 impression recorded within the last 2 hours
+    }
+
+    @Test
+    fun testPerHourWithoutImpressions() {
+        // Arrange
+        val campaignId = "campaign123"
+        val currentTimestamp = 1000L // Replace with a desired timestamp
+        val hoursOffset = 2 // Hours to subtract
+
+        `when`(clock.currentTimeSeconds()).thenReturn(currentTimestamp)
+
+        // Act
+        val result = impressionManager.perHour(campaignId, hoursOffset)
+
+        // Assert
+        assertEquals(0, result) // Expecting 0 impressions within the last 2 hours
+    }
+
+    @Test
+    fun testPerDayWithImpressions() {
+        // Arrange
+        val campaignId = "campaign123"
+        val daysOffset = 2 // Days to subtract
+
+        val currentDate = Calendar.getInstance()
+        //currentDate.set(2023, Calendar.SEPTEMBER, 25) // Set the currentDate to some past date to fail this test
+
+        `when`(clock.currentTimeSeconds()).thenReturn(TimeUnit.MILLISECONDS.toSeconds(currentDate.timeInMillis))
+
+        // Act
+        impressionManager.recordImpression(campaignId) // Record an impression
+        val result = impressionManager.perDay("campaign123", daysOffset)
+
+        // Assert
+        assertEquals(1, result) // Expecting 1 impression recorded within the last 2 days
+    }
+
+    @Test
+    fun testPerDayWithoutImpressions() {
+        // Arrange
+        val campaignId = "campaign123"
+        val daysOffset = 2 // Days to subtract
+
+        val currentDate = Calendar.getInstance()
+        `when`(clock.currentTimeSeconds()).thenReturn(TimeUnit.MILLISECONDS.toSeconds(currentDate.timeInMillis))
+
+        // Act
+        val result = impressionManager.perDay(campaignId, daysOffset)
+
+        // Assert
+        assertEquals(0, result) // Expecting 0 impressions within the last 2 days
+    }
+
+    @Test
+    fun testPerWeekWithImpressions() {
+        // Arrange
+        val campaignId = "campaign123"
+        val weeksOffset = 1 // Weeks to subtract
+        val currentDate = Calendar.getInstance()
+        currentDate.set(
+            2023, Calendar.SEPTEMBER, 24
+        ) // Set the currentDate to some past date to fail this test
+
+        `when`(clock.currentTimeSeconds()).thenReturn(TimeUnit.MILLISECONDS.toSeconds(currentDate.timeInMillis))
+
+        // Act
+        impressionManager.recordImpression(campaignId) // Record an impression
+        val result = impressionManager.perWeek(campaignId, weeksOffset)
+
+        // Assert
+        assertEquals(1, result) // Expecting 1 impression recorded within the last 2 weeks
+    }
+
+    @Test
+    fun testPerWeekWithoutImpressions() {
+        // Arrange
+        val campaignId = "campaign123"
+        val weeksOffset = 2 // Weeks to subtract
+        val currentDate = Calendar.getInstance()
+        currentDate.set(2023, Calendar.SEPTEMBER, 24)
+
+        `when`(clock.currentTimeSeconds()).thenReturn(TimeUnit.MILLISECONDS.toSeconds(currentDate.timeInMillis))
+
+        // Act
+        val result = impressionManager.perWeek(campaignId, weeksOffset)
+
+        // Assert
+        assertEquals(0, result) // Expecting 0 impressions within the last 2 weeks
+    }
+
+    @Test
+    fun testClearSessionData() {
+        // Arrange
+        val campaignId = "campaign123"
+
+        // Record an impression to ensure sessionImpressions is not empty
+        impressionManager.recordImpression(campaignId)
+
+        // Verify that sessionImpressions is not empty initially
+        val initialSessionImpressionsSize = impressionManager.perSessionTotal()
+        assert(initialSessionImpressionsSize > 0)
+
+        // Act
+        impressionManager.clearSessionData()
+
+        // Assert
+        val clearedSessionImpressionsSize = impressionManager.perSessionTotal()
+        assertEquals(0, clearedSessionImpressionsSize) // Expecting sessionImpressions to be cleared
+    }
+}
