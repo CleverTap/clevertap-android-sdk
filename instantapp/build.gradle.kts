e7084be6b06e2f134d9cd9ec1a334370f6fdb108
lalit <lalitkumar@clevertap.com>
2023-10-07 00:16:55 +0530

    version bump

--- instantapp/build.gradle.kts
+++ instantapp/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.clevertap.demo"
         minSdk = 21
         targetSdk = 33
-        versionCode = 10500
+        versionCode = 10555
         versionName = "1.6.0-instant"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
