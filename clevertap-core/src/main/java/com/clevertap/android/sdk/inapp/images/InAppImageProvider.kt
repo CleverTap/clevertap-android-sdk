c7aa312398b3f5291f483325017cf93916556f6c
lalit <lalitkumar@clevertap.com>
2023-10-30 17:38:26 +0530

    task(SDK-3362) image provider
    - fetch image functionality
    - provides bitmap/byte data as needed
    - made method inline to get generic type

--- clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/images/InAppImageProvider.kt
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/images/InAppImageProvider.kt
@@ -2,16 +2,18 @@ package com.clevertap.android.sdk.inapp.images
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import com.clevertap.android.sdk.ILogger
 import com.clevertap.android.sdk.bitmap.BitmapDownloadRequest
 import com.clevertap.android.sdk.bitmap.HttpBitmapLoader
 import com.clevertap.android.sdk.bitmap.HttpBitmapLoader.HttpBitmapOperation
 import com.clevertap.android.sdk.network.DownloadedBitmap
 import com.clevertap.android.sdk.utils.CTCaches
+import java.io.ByteArrayOutputStream
 
 class InAppImageProvider(
-    private val context: Context,
-    private val logger: ILogger? = null
+    val context: Context,
+    val logger: ILogger? = null
 ) {
 
     private val ctCaches = CTCaches.instance(logger = logger)
@@ -25,14 +27,14 @@ class InAppImageProvider(
         imageDiskCache.add(cacheKey, bytes)
     }
 
-    fun cachedImage(cacheKey: String) {
+    fun cachedImage(cacheKey: String): Bitmap? {
 
         // Try in memory
         val imageMemoryCache = ctCaches.imageCache()
         val bitmap = imageMemoryCache.get(cacheKey)
 
         if (bitmap != null) {
-            return
+            return bitmap
         }
 
         // Try disk
@@ -40,21 +42,45 @@ class InAppImageProvider(
         val file = imageDiskCache.get(cacheKey)
 
         if (file != null) {
-            return
+            val options = BitmapFactory.Options()
+            options.inJustDecodeBounds = true
+            // use options to scale down/optimise bitmap if needed
+            val op = BitmapFactory.decodeFile(file.absolutePath)
+            return op
+            /*val clazz = T::class.java
+            if (clazz.isAssignableFrom(Bitmap::class.java)) {
+                val op = BitmapFactory.decodeFile(file.absolutePath, null)
+                return op as T
+            } else if (clazz.isAssignableFrom(ByteArray::class.java)) {
+                file.inputStream()
+            }*/
         }
-
+        return null
     }
 
     /**
      * Function that would fetch and cache bitmap image into Memory and File cache and return it.
      * If image is found in cache, the cached image is returned.
      */
-    fun fetchImage(url: String) {
-        val request = BitmapDownloadRequest(url)
-        val downloadedBitmap: DownloadedBitmap = HttpBitmapLoader.getHttpBitmap(
-            bitmapOperation = HttpBitmapOperation.DOWNLOAD_INAPP_BITMAP,
-            bitmapDownloadRequest = request
-        )
+    inline fun <reified T> fetchImage(url: String): T? {
+
+        val cachedImage: Bitmap? = cachedImage(url)
+        val clazz = T::class.java
+
+        if (cachedImage != null) {
+            return if (clazz.isAssignableFrom(Bitmap::class.java)) {
+                cachedImage as? T
+            } else if (clazz.isAssignableFrom(ByteArray::class.java)) {
+                val stream = ByteArrayOutputStream()
+                cachedImage.compress(Bitmap.CompressFormat.PNG, 100, stream)
+                val byteArray = stream.toByteArray()
+                byteArray as? T
+            } else {
+                null
+            }
+        }
+
+        val downloadedBitmap = makeApiCallForInAppBitmap(url = url)
 
         when (downloadedBitmap.status) {
 
@@ -63,8 +89,25 @@ class InAppImageProvider(
             }
             else -> {
                 logger?.verbose("There was a problem fetching data for bitmap")
+                return null
             }
         }
+
+        return if (clazz.isAssignableFrom(Bitmap::class.java)) {
+            downloadedBitmap.bitmap as? T
+        } else if (clazz.isAssignableFrom(ByteArray::class.java)) {
+            downloadedBitmap.bytes as? T
+        } else {
+            null
+        }
+    }
+
+    fun makeApiCallForInAppBitmap(url: String): DownloadedBitmap {
+        val request = BitmapDownloadRequest(url)
+        return HttpBitmapLoader.getHttpBitmap(
+            bitmapOperation = HttpBitmapOperation.DOWNLOAD_INAPP_BITMAP,
+            bitmapDownloadRequest = request
+        )
     }
 
 }
