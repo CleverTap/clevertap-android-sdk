deafac192784897746f3620c23efdfbb849d4176
CTLalit <lalitkumar@clevertap.com>
2023-11-17 13:08:35 +0530

    Async -> Limited parallelism
    - used limited parallelism instead of using async
    - this will give desired functionality to limit no of threads

--- clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/images/preload/InAppImagePreloaderCoroutine.kt
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/images/preload/InAppImagePreloaderCoroutine.kt
@@ -6,10 +6,8 @@ import com.clevertap.android.sdk.utils.CtDefaultDispatchers
 import com.clevertap.android.sdk.utils.DispatcherProvider
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
 
 internal class InAppImagePreloaderCoroutine @JvmOverloads constructor(
@@ -19,37 +17,25 @@ internal class InAppImagePreloaderCoroutine @JvmOverloads constructor(
         override val config: InAppImagePreloadConfig = InAppImagePreloadConfig.default()
 ) : InAppImagePreloaderStrategy {
 
-    private var job: Job? = null
+    private val jobs: MutableList<Job> = mutableListOf()
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun preloadImages(urls: List<String>) {
 
         val handler = CoroutineExceptionHandler { _, throwable ->
             logger?.verbose("Cancelled image pre fetch \n ${throwable.stackTrace}")
         }
-        val scope = CoroutineScope(dispatchers.io())
-        job = scope.launch(context = handler) {
-            val list = mutableListOf<Deferred<Unit?>>()
-            urls.chunked(
-                config.parallelDownloads
-            ).forEach { chunks ->
-                logger?.verbose("Downloading image chunk with size ${chunks.size}")
-                chunks.forEach { url ->
-                    if (inAppImageProvider.isImageCached(url = url).not()) {
-                        // start async download if not found in cache
-                        val async: Deferred<Unit> = async {
-                            inAppImageProvider.fetchInAppImage(url)
-                        }
-                        list.add(async)
-                    } else {
-                        logger?.verbose("Found cached image for $url")
-                    }
-                }
-                list.awaitAll()
+        val scope = CoroutineScope(dispatchers.io().limitedParallelism(config.parallelDownloads))
+
+        urls.forEach { url ->
+            val job = scope.launch(handler) {
+                inAppImageProvider.fetchInAppImage(url)
             }
+            jobs.add(job)
         }
     }
 
     override fun cleanup() {
-        job?.cancel()
+        jobs.map { job -> job.cancel() }
     }
 }
