303bef0135ee64dde9663313e4ff9321da02f79c
Shivam Sharma <shivam.sharma@clevertap.com>
2023-12-04 21:47:39 +0530

    feat(inapp) - add unit tests for recordImpression(), perSession() and perSessionTotal() in EvaluationManagerTest SDK-3459

--- clevertap-core/src/test/java/com/clevertap/android/sdk/inapp/ImpressionManagerTest.kt
+++ clevertap-core/src/test/java/com/clevertap/android/sdk/inapp/ImpressionManagerTest.kt
@@ -1,10 +1,26 @@
 package com.clevertap.android.sdk.inapp
 
+import com.clevertap.android.sdk.DeviceInfo
+import com.clevertap.android.sdk.StoreProvider
+import com.clevertap.android.sdk.inapp.store.preference.ImpressionStore
+import com.clevertap.android.sdk.inapp.store.preference.StoreRegistry
+import com.clevertap.android.sdk.utils.Clock
 import com.clevertap.android.shared.test.BaseTestCase
+import org.junit.*
+import org.mockito.*
+import org.mockito.Mockito.*
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import kotlin.test.assertEquals
 
 class ImpressionManagerTest : BaseTestCase() {
-   /* @Mock
-    private lateinit var clock: Clock
+
+    @Mock
+    private lateinit var clock: FakeClock
+
+    @Mock
+    private lateinit var deviceInfo: DeviceInfo
 
     private lateinit var impressionManager: ImpressionManager
 
@@ -12,22 +28,48 @@ class ImpressionManagerTest : BaseTestCase() {
         super.setUp()
         MockitoAnnotations.openMocks(this)
 
+        val storeRegistry = StoreRegistry()
+        clock = FakeClock()
 
         impressionManager = ImpressionManager(
-            impressionStore = StoreProvider.getInstance().provideImpressionStore(application, DeviceInfo(),"accountId")
+            storeRegistry = storeRegistry,
             clock = clock,
             locale = Locale.US
         )
+
+        `when`(deviceInfo.deviceID).thenReturn("device_id")
+
+        val impStore: ImpressionStore = StoreProvider.getInstance().provideImpressionStore(
+            appCtx, deviceInfo,
+            "account_id"
+        )
+
+        storeRegistry.impressionStore = impStore
+    }
+
+    class FakeClock : Clock {
+
+        override fun currentTimeMillis(): Long {
+            val seconds = 1000L
+            return seconds * 1000
+        }
+
+        override fun newDate(): Date {
+            val dateFormatter = SimpleDateFormat("yyyyMMdd", Locale.getDefault())
+            return dateFormatter.parse("20230126")!!// January 26, 2023
+        }
     }
 
     @Test
-    fun testRecordImpression() {
+    fun `recordImpression should increase sessionImpressionsTotal`() {
         // Arrange
         val campaignId = "campaign123"
-        val currentTime = 123456L // Replace with a desired timestamp
+//        val currentTime = 123456L // Replace with a desired timestamp
+//
+//        // Mock the clock to return a fixed timestamp
+//        `when`(clock.currentTimeSeconds()).thenReturn(currentTime)
 
-        // Mock the clock to return a fixed timestamp
-        `when`(clock.currentTimeSeconds()).thenReturn(currentTime)
+        assertEquals(0, impressionManager.perSessionTotal())
 
         // Act
         impressionManager.recordImpression(campaignId)
@@ -37,17 +79,14 @@ class ImpressionManagerTest : BaseTestCase() {
     }
 
     @Test
-    fun testPerSessionWithCampaignIdPresent() {
+    fun `perSession should return correct impression count when campaignId is present`() {
         // Arrange
         val campaignId = "campaign123"
 
-        // Create a sessionImpressions map with a campaignId and associated list
-        val sessionImpressions = mutableMapOf(
-            campaignId to mutableListOf(1L, 2L, 3L)
-        )
-
-        // Set the sessionImpressions map in the impressionManager
-        impressionManager.setSessionImpressions(sessionImpressions)
+        // Record impressions for the campaign in the session
+        impressionManager.recordImpression(campaignId)
+        impressionManager.recordImpression(campaignId)
+        impressionManager.recordImpression(campaignId)
 
         // Act
         val result = impressionManager.perSession(campaignId)
@@ -57,7 +96,7 @@ class ImpressionManagerTest : BaseTestCase() {
     }
 
     @Test
-    fun testPerSessionWithCampaignIdMissing() {
+    fun `perSession should return 0 when campaignId is missing`() {
         // Arrange
         val campaignId = "campaign123"
 
@@ -69,7 +108,7 @@ class ImpressionManagerTest : BaseTestCase() {
     }
 
     @Test
-    fun testPerSessionTotalWithInitialValue() {
+    fun `perSessionTotal should return 0 with initial value`() {
         // Act
         val result = impressionManager.perSessionTotal()
 
@@ -78,7 +117,7 @@ class ImpressionManagerTest : BaseTestCase() {
     }
 
     @Test
-    fun testPerSessionTotalAfterRecordImpressions() {
+    fun `perSessionTotal should return correct count after recording impressions`() {
         // Arrange
         val campaignId = "campaign123"
 
@@ -93,201 +132,4 @@ class ImpressionManagerTest : BaseTestCase() {
         // Assert
         assertEquals(3, result) // Expecting the total number of impressions recorded
     }
-
-    @Test
-    fun testPerSecondWithImpressions() {
-        // Arrange
-        val campaignId = "campaign123"
-        val currentTimeSeconds = 1000L
-        val seconds = 10
-
-        `when`(clock.currentTimeSeconds()).thenReturn(currentTimeSeconds)
-
-        // Record one impression
-        impressionManager.recordImpression(campaignId)
-
-        // Act
-        val result = impressionManager.perSecond(campaignId, seconds)
-
-        // Assert
-        assertEquals(1, result) // Expecting 1 impression within the last 10 seconds
-    }
-
-    @Test
-    fun testPerSecondWithoutImpressions() {
-        // Arrange
-        val campaignId = "campaign123"
-        val currentTimeSeconds = 1000L
-        val seconds = 10
-
-        `when`(clock.currentTimeSeconds()).thenReturn(currentTimeSeconds)
-
-        // No impressions recorded, so the result should be 0
-
-        // Act
-        val result = impressionManager.perSecond(campaignId, seconds)
-
-        // Assert
-        assertEquals(0, result) // Expecting 0 impressions within the last 10 seconds
-    }
-
-    @Test
-    fun testPerMinuteWithImpressions() {
-        // Arrange
-        val campaignId = "campaign123"
-        val currentTimestamp = 1000L // Replace with a desired timestamp
-        val minutesOffset = 5 // Minutes to subtract
-
-        `when`(clock.currentTimeSeconds()).thenReturn(currentTimestamp)
-
-        // Act
-        impressionManager.recordImpression(campaignId) // Record an impression
-        val result = impressionManager.perMinute(campaignId, minutesOffset)
-
-        // Assert
-        assertEquals(1, result) // Expecting 1 impression recorded within the last 5 minutes
-    }
-
-    @Test
-    fun testPerMinuteWithoutImpressions() {
-        // Arrange
-        val campaignId = "campaign123"
-        val currentTimestamp = 1000L // Replace with a desired timestamp
-        val minutesOffset = 5 // Minutes to subtract
-
-        `when`(clock.currentTimeSeconds()).thenReturn(currentTimestamp)
-
-        // No impressions recorded, so the result should be 0
-
-        // Act
-        val result = impressionManager.perMinute(campaignId, minutesOffset)
-
-        // Assert
-        assertEquals(0, result) // Expecting 0 impressions within the last 5 minutes
-    }
-
-    @Test
-    fun testPerHourWithImpressions() {
-        // Arrange
-        val campaignId = "campaign123"
-        val currentTimestamp = 1000L // Replace with a desired timestamp
-        val hoursOffset = 2 // Hours to subtract
-
-        `when`(clock.currentTimeSeconds()).thenReturn(currentTimestamp)
-
-        // Act
-        impressionManager.recordImpression(campaignId) // Record an impression
-        val result = impressionManager.perHour(campaignId, hoursOffset)
-
-        // Assert
-        assertEquals(1, result) // Expecting 1 impression recorded within the last 2 hours
-    }
-
-    @Test
-    fun testPerHourWithoutImpressions() {
-        // Arrange
-        val campaignId = "campaign123"
-        val currentTimestamp = 1000L // Replace with a desired timestamp
-        val hoursOffset = 2 // Hours to subtract
-
-        `when`(clock.currentTimeSeconds()).thenReturn(currentTimestamp)
-
-        // Act
-        val result = impressionManager.perHour(campaignId, hoursOffset)
-
-        // Assert
-        assertEquals(0, result) // Expecting 0 impressions within the last 2 hours
-    }
-
-    @Test
-    fun testPerDayWithImpressions() {
-        // Arrange
-        val campaignId = "campaign123"
-        val daysOffset = 2 // Days to subtract
-
-        val currentDate = Calendar.getInstance()
-        //currentDate.set(2023, Calendar.SEPTEMBER, 25) // Set the currentDate to some past date to fail this test
-
-        `when`(clock.currentTimeSeconds()).thenReturn(TimeUnit.MILLISECONDS.toSeconds(currentDate.timeInMillis))
-
-        // Act
-        impressionManager.recordImpression(campaignId) // Record an impression
-        val result = impressionManager.perDay("campaign123", daysOffset)
-
-        // Assert
-        assertEquals(1, result) // Expecting 1 impression recorded within the last 2 days
-    }
-
-    @Test
-    fun testPerDayWithoutImpressions() {
-        // Arrange
-        val campaignId = "campaign123"
-        val daysOffset = 2 // Days to subtract
-
-        val currentDate = Calendar.getInstance()
-        `when`(clock.currentTimeSeconds()).thenReturn(TimeUnit.MILLISECONDS.toSeconds(currentDate.timeInMillis))
-
-        // Act
-        val result = impressionManager.perDay(campaignId, daysOffset)
-
-        // Assert
-        assertEquals(0, result) // Expecting 0 impressions within the last 2 days
-    }
-
-    @Test
-    fun testPerWeekWithImpressions() {
-        // Arrange
-        val campaignId = "campaign123"
-        val weeksOffset = 1 // Weeks to subtract
-        val currentDate = Calendar.getInstance()
-        *//*currentDate.set(
-            2023, Calendar.SEPTEMBER, 24
-        )*//* // Set the currentDate to some past date to fail this test
-
-        `when`(clock.currentTimeSeconds()).thenReturn(TimeUnit.MILLISECONDS.toSeconds(currentDate.timeInMillis))
-
-        // Act
-        impressionManager.recordImpression(campaignId) // Record an impression
-        val result = impressionManager.perWeek(campaignId, weeksOffset)
-
-        // Assert
-        assertEquals(1, result) // Expecting 1 impression recorded within the last 2 weeks
-    }
-
-    @Test
-    fun testPerWeekWithoutImpressions() {
-        // Arrange
-        val campaignId = "campaign123"
-        val weeksOffset = 2 // Weeks to subtract
-        val currentDate = Calendar.getInstance()
-        currentDate.set(2023, Calendar.SEPTEMBER, 24)
-
-        `when`(clock.currentTimeSeconds()).thenReturn(TimeUnit.MILLISECONDS.toSeconds(currentDate.timeInMillis))
-
-        // Act
-        val result = impressionManager.perWeek(campaignId, weeksOffset)
-
-        // Assert
-        assertEquals(0, result) // Expecting 0 impressions within the last 2 weeks
-    }
-
-    @Test
-    fun testClearSessionData() {
-        // Arrange
-        val campaignId = "campaign123"
-
-        // Record an impression to ensure sessionImpressions is not empty
-        impressionManager.recordImpression(campaignId)
-
-        // Verify that sessionImpressions is not empty initially
-        val initialSessionImpressionsSize = impressionManager.perSessionTotal()
-        assert(initialSessionImpressionsSize > 0)
-
-        // Act
-        impressionManager.clearSessionData()
-
-        // Assert
-        val clearedSessionImpressionsSize = impressionManager.perSessionTotal()
-        assertEquals(0, clearedSessionImpressionsSize) // Expecting sessionImpressions to be cleared
-    }*/
 }
