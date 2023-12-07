d6611e7bec5ad6c4c8131e2abf25325af2f22eaf
Shivam Sharma <shivam.sharma@clevertap.com>
2023-12-07 15:24:45 +0530

    feat(inapp) - create unit test file for InAppResponseAdapter

---
+++ clevertap-core/src/test/java/com/clevertap/android/sdk/inapp/InAppResponseAdapterTest.kt
@@ -0,0 +1,12 @@
+package com.clevertap.android.sdk.inapp
+
+import com.clevertap.android.shared.test.BaseTestCase
+import org.mockito.*
+
+class InAppResponseAdapterTest : BaseTestCase() {
+
+    override fun setUp() {
+        super.setUp()
+        MockitoAnnotations.openMocks(this)
+    }
+}
