5a7f08c0e6dbe42aa7e8fd51c3aca7f5657f5bb1
CTLalit <lalitkumar@clevertap.com>
2023-12-11 13:14:56 +0530

    Test: InApp Repo test
    - tested fetch all images function

---
+++ clevertap-core/src/test/java/com/clevertap/android/sdk/inapp/images/repo/InAppImageRepoTest.kt
@@ -0,0 +1,36 @@
+package com.clevertap.android.sdk.inapp.images.repo
+
+import com.clevertap.android.sdk.inapp.images.cleanup.InAppCleanupStrategy
+import com.clevertap.android.sdk.inapp.images.preload.InAppImagePreloaderStrategy
+import com.clevertap.android.sdk.inapp.store.preference.InAppAssetsStore
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+
+
+class InAppImageRepoTest {
+
+    private val inAppImageCleanupStrategy = mockk<InAppCleanupStrategy>(relaxed = true)
+    private val preloaderStrategy = mockk<InAppImagePreloaderStrategy>(relaxed = true)
+    private val inAppAssetStore = mockk<InAppAssetsStore>(relaxed = true)
+
+    private val inAppImageRepoImpl = InAppImageRepoImpl(
+        cleanupStrategy = inAppImageCleanupStrategy,
+        preloaderStrategy = preloaderStrategy,
+        inAppAssetsStore = inAppAssetStore
+    )
+
+    @Test
+    fun `fetch all images use case`() {
+        val urls = listOf("url1", "url2", "url3")
+        inAppImageRepoImpl.fetchAllImages(urls)
+
+        every { inAppImageRepoImpl.fetchAllImages(any()) } returns Unit
+        every { preloaderStrategy.preloadImages(urls, any()) } returns Unit
+
+        verify {
+            preloaderStrategy.preloadImages(urls, any())
+        }
+    }
+}
