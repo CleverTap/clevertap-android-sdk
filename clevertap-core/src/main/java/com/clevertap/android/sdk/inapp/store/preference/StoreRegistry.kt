48eca1d064232cab3d560b0f40bf3e0a1b89b479
piyush.kukadiya <piyush.kukadiya@clevertap.com>
2024-01-03 17:25:58 +0530

    chore(inapp) - add java doc for StoreRegistry SDK-3565

--- clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/store/preference/StoreRegistry.kt
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/store/preference/StoreRegistry.kt
@@ -1,5 +1,16 @@
 package com.clevertap.android.sdk.inapp.store.preference
 
+/**
+ * The `StoreRegistry` data class acts as a registry for various stores related to In-App messaging.
+ * It holds instances of different store types, allowing easy access to these stores throughout the application.
+ *
+ * @property inAppStore An instance of [InAppStore] for storing In-App messages.
+ * @property impressionStore An instance of [ImpressionStore] for tracking impressions of In-App messages.
+ * @property legacyInAppStore An instance of [LegacyInAppStore] for storing legacy In-App messages.
+ * @property inAppAssetsStore An instance of [InAppAssetsStore] for handling In-App assets storage.
+ *
+ * @constructor Creates a StoreRegistry with optional instances of different stores.
+ */
 data class StoreRegistry constructor(
     var inAppStore: InAppStore? = null,
     var impressionStore: ImpressionStore? = null,
