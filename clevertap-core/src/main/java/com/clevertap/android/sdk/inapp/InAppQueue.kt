2d01811eaa82ca93f137efb58d757850661c7092
piyush.kukadiya <piyush.kukadiya@clevertap.com>
2024-01-03 17:29:07 +0530

    chore(inapp) - add java doc for InAppQueue SDK-3565

--- clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/InAppQueue.kt
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/InAppQueue.kt
@@ -7,11 +7,29 @@ import com.clevertap.android.sdk.inapp.store.preference.StoreRegistry
 import org.json.JSONArray
 import org.json.JSONObject
 
+/**
+ * The `InAppQueue` class manages the queue of ss In-App messages.
+ * It provides methods to enqueue, dequeue, and retrieve the length of the queue.
+ *
+ * <p>
+ * The queue is stored in the [InAppStore] under the server-side mode, ensuring persistent storage.
+ * </p>
+ *
+ * @property config The configuration for the CleverTap SDK instance.
+ * @property storeRegistry The registry providing access to different stores, including [InAppStore].
+ *
+ * @constructor Creates an InAppQueue with the given configuration and store registry.
+ */
 class InAppQueue(
     private val config: CleverTapInstanceConfig,
     private val storeRegistry: StoreRegistry
 ) {
 
+    /**
+     * Enqueues a single In-App message in the queue.
+     *
+     * @param jsonObject The In-App message to enqueue.
+     */
     @WorkerThread
     @Synchronized
     fun enqueue(jsonObject: JSONObject) {
@@ -20,6 +38,11 @@ class InAppQueue(
         saveQueue(currentQueue)
     }
 
+    /**
+     * Enqueues multiple In-App messages from a JSONArray in the queue.
+     *
+     * @param jsonArray The JSONArray containing multiple In-App messages to enqueue.
+     */
     @WorkerThread
     @Synchronized
     fun enqueueAll(jsonArray: JSONArray) {
@@ -37,6 +60,11 @@ class InAppQueue(
         saveQueue(currentQueue)
     }
 
+    /**
+     * Dequeues and removes the first In-App message from the queue.
+     *
+     * @return The dequeued In-App message, or null if the queue is empty.
+     */
     @WorkerThread
     @Synchronized
     fun dequeue(): JSONObject? {
@@ -49,6 +77,11 @@ class InAppQueue(
         return removedObject as JSONObject
     }
 
+    /**
+     * Gets the current length of the In-App queue.
+     *
+     * @return The length of the In-App queue.
+     */
     @WorkerThread
     @Synchronized
     fun getQueueLength(): Int {
@@ -56,10 +89,20 @@ class InAppQueue(
         return currentQueue.length()
     }
 
+    /**
+     * Retrieves the server-side In-App queue from the [InAppStore].
+     *
+     * @return The current server-side In-App queue.
+     */
     private fun getQueue(): JSONArray {
         val inAppStore = storeRegistry.inAppStore ?: return JSONArray()
         return inAppStore.readServerSideInApps()
     }
 
+    /**
+     * Saves the modified server-side In-App queue back to the [InAppStore].
+     *
+     * @param queue The updated server-side In-App queue.
+     */
     private fun saveQueue(queue: JSONArray) = storeRegistry.inAppStore?.storeServerSideInApps(queue)
 }
