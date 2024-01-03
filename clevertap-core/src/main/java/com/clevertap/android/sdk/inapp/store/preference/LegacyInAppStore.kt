78afb79e74a0d64866389da905f3c16a76a0ac65
piyush.kukadiya <piyush.kukadiya@clevertap.com>
2024-01-03 17:28:05 +0530

    chore(inapp) - add java doc for LegacyInAppStore SDK-3565

--- clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/store/preference/LegacyInAppStore.kt
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/store/preference/LegacyInAppStore.kt
@@ -6,18 +6,38 @@ import com.clevertap.android.sdk.store.preference.ICTPreference
 import org.json.JSONArray
 import org.json.JSONException
 
+/**
+ * The `LegacyInAppStore` class manages the storage and retrieval of legacy In-App messages.
+ * Legacy In-App messages are stored in the shared preferences named "WizRocket" with key "inApp" concatenated
+ * with the provided account identifier. This class is used for handling In-App messages in a legacy manner.
+ *
+ * @property ctPreference The instance of the preference manager interface (`ICTPreference`) for handling preferences.
+ * @property accountId The unique account identifier used for creating a specific preference key.
+ */
 class LegacyInAppStore(private val ctPreference: ICTPreference, accountId: String) {
 
     companion object {
+
         private const val ASSETS_CLEANUP_TS_KEY = "last_assets_cleanup"
     }
 
+    // Key for storing and retrieving legacy In-App messages
     private val inAppKey = Constants.INAPP_KEY.concatIfNotNull(accountId, ":")
 
+    /**
+     * Stores legacy In-App messages in the shared preferences.
+     *
+     * @param inApps The array of legacy In-App messages to be stored.
+     */
     fun storeInApps(inApps: JSONArray) {
         ctPreference.writeStringImmediate(inAppKey!!, inApps.toString())
     }
 
+    /**
+     * Reads and retrieves legacy In-App messages from the shared preferences.
+     *
+     * @return An array of legacy In-App messages.
+     */
     fun readInApps(): JSONArray {
         val storedInApps = ctPreference.readString(inAppKey!!, "[]")
         return try {
@@ -27,6 +47,9 @@ class LegacyInAppStore(private val ctPreference: ICTPreference, accountId: Strin
         }
     }
 
+    /**
+     * Removes the stored legacy In-App messages from the shared preferences.
+     */
     fun removeInApps() {
         ctPreference.remove(inAppKey!!)
     }
@@ -35,7 +58,7 @@ class LegacyInAppStore(private val ctPreference: ICTPreference, accountId: Strin
         ctPreference.writeLong(ASSETS_CLEANUP_TS_KEY, ts)
     }
 
-    fun lastCleanupTs() : Long {
+    fun lastCleanupTs(): Long {
         return ctPreference.readLong(ASSETS_CLEANUP_TS_KEY, 0)
     }
 }
