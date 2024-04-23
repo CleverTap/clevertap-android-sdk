91e22248f8273ee3e69273b93c4a608af1f5adb7
anush <anush@clevertap.com>
2024-04-23 15:07:16 +0530

    task(SDK-3795) - Fixes NPE
    - Adds null check before string comparison

--- clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/InAppController.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/InAppController.java
@@ -706,7 +706,7 @@ public class InAppController implements CTInAppNotification.CTInAppNotificationL
             return;
         }
 
-        boolean isHtmlType = inAppNotification.getType().equals(Constants.KEY_CUSTOM_HTML);
+        boolean isHtmlType = inAppNotification.getType() != null && inAppNotification.getType().equals(Constants.KEY_CUSTOM_HTML);
         if (isHtmlType && !NetworkManager.isNetworkOnline(context)) {
             Logger.d(config.getAccountId(),
                     "Not showing HTML InApp due to no internet. An active internet connection is required to display the HTML InApp");
