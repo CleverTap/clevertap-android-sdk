386286dfe61e189938009517679ff18ea0ef0a8a
CTLalit <lalitkumar@clevertap.com>
2024-05-30 18:57:56 +0530

    Feat: Exoplayer code extraction from fragment
    - removed exoplay and styledplayerview instance in other handle
    - the handle provides methods which will be extracted in interface

--- clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/CTInAppNativeInterstitialFragment.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/CTInAppNativeInterstitialFragment.java
@@ -4,14 +4,13 @@ package com.clevertap.android.sdk.inapp;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -33,27 +32,10 @@ import androidx.core.content.res.ResourcesCompat;
 import com.clevertap.android.sdk.R;
 import com.clevertap.android.sdk.customviews.CloseImageView;
 import com.clevertap.android.sdk.gif.GifImageView;
-import com.google.android.exoplayer2.ExoPlayer;
-import com.google.android.exoplayer2.MediaItem;
-import com.google.android.exoplayer2.Player;
-import com.google.android.exoplayer2.source.hls.HlsMediaSource;
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.ExoTrackSelection;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.ui.StyledPlayerView;
-import com.google.android.exoplayer2.upstream.BandwidthMeter;
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSource;
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
-import com.google.android.exoplayer2.upstream.TransferListener;
-import com.google.android.exoplayer2.util.Util;
+import com.clevertap.android.sdk.video.ExoplayerHandle;
 import java.util.ArrayList;
 
 public class CTInAppNativeInterstitialFragment extends CTInAppBaseFullNativeFragment {
-
-    private static long mediaPosition = 0;
 
     private boolean exoPlayerFullscreen = false;
 
@@ -63,15 +45,13 @@ public class CTInAppNativeInterstitialFragment extends CTInAppBaseFullNativeFrag
 
     private GifImageView gifImageView;
 
-    private ExoPlayer player;
-
-    private StyledPlayerView playerView;
+    private ExoplayerHandle handle = new ExoplayerHandle();
 
     private RelativeLayout relativeLayout;
 
     private FrameLayout videoFrameLayout;
 
-    private ViewGroup.LayoutParams videoFramelayoutParams, playerViewLayoutParams, imageViewLayoutParams;
+    private ViewGroup.LayoutParams imageViewLayoutParams;
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Nullable
@@ -171,7 +151,6 @@ public class CTInAppNativeInterstitialFragment extends CTInAppBaseFullNativeFrag
                     gifImageView.startAnimation();
                 }
             } else if (media.isVideo()) {
-                initFullScreenDialog();
                 prepareMedia();
                 playMedia();
             } else if (media.isAudio()) {
@@ -247,7 +226,7 @@ public class CTInAppNativeInterstitialFragment extends CTInAppBaseFullNativeFrag
     @Override
     public void onResume() {
         super.onResume();
-        if (player == null && (inAppNotification.hasStreamMedia())) {
+        if (inAppNotification.hasStreamMedia()) {
             prepareMedia();
             playMedia();
         }
@@ -262,12 +241,8 @@ public class CTInAppNativeInterstitialFragment extends CTInAppBaseFullNativeFrag
         if (exoPlayerFullscreen) {
             closeFullscreenDialog();
         }
-        if (player != null) {
-            mediaPosition = player.getCurrentPosition();
-            player.stop();
-            player.release();
-            player = null;
-        }
+        handle.savePosition();
+        handle.pause();
     }
 
     @Override
@@ -276,11 +251,7 @@ public class CTInAppNativeInterstitialFragment extends CTInAppBaseFullNativeFrag
         if (gifImageView != null) {
             gifImageView.clear();
         }
-        if (player != null) {
-            player.stop();
-            player.release();
-        }
-
+        handle.pause();
     }
 
     @Override
@@ -294,69 +265,84 @@ public class CTInAppNativeInterstitialFragment extends CTInAppBaseFullNativeFrag
         if (gifImageView != null) {
             gifImageView.clear();
         }
-        if (player != null) {
-            player.stop();
-            player.release();
-            player = null;
-        }
-    }
-
-    private void closeFullscreenDialog() {
-        ((ViewGroup) playerView.getParent()).removeView(playerView);
-        playerView.setLayoutParams(playerViewLayoutParams);
-        ((FrameLayout) videoFrameLayout.findViewById(R.id.video_frame)).addView(playerView);
-        fullScreenIcon.setLayoutParams(imageViewLayoutParams);
-        ((FrameLayout) videoFrameLayout.findViewById(R.id.video_frame)).addView(fullScreenIcon);
-        videoFrameLayout.setLayoutParams(videoFramelayoutParams);
-        ((RelativeLayout) relativeLayout.findViewById(R.id.interstitial_relative_layout)).addView(videoFrameLayout);
-        exoPlayerFullscreen = false;
-        fullScreenDialog.dismiss();
-        fullScreenIcon.setImageDrawable(
-                ContextCompat.getDrawable(this.context, R.drawable.ct_ic_fullscreen_expand));
+        handle.pause();
     }
 
     private void disableFullScreenButton() {
         fullScreenIcon.setVisibility(View.GONE);
     }
 
-    private void initFullScreenDialog() {
-        fullScreenDialog = new Dialog(this.context, android.R.style.Theme_Black_NoTitleBar_Fullscreen) {
-            public void onBackPressed() {
-                if (exoPlayerFullscreen) {
-                    closeFullscreenDialog();
-                }
-                super.onBackPressed();
-            }
-        };
+    private void closeFullscreenDialog() {
+        View playerView = handle.player();
+
+        handle.switchToFullScreen(false);
+
+        fullScreenIcon.setLayoutParams(imageViewLayoutParams);
+        videoFrameLayout.addView(playerView);
+        videoFrameLayout.addView(fullScreenIcon);
+        exoPlayerFullscreen = false;
+
+        // dismiss full screen dialog
+        fullScreenDialog.dismiss();
+        fullScreenIcon.setImageDrawable(ContextCompat.getDrawable(this.context, R.drawable.ct_ic_fullscreen_expand));
     }
 
     private void openFullscreenDialog() {
+        View playerView = handle.player();
+
         imageViewLayoutParams = fullScreenIcon.getLayoutParams();
-        playerViewLayoutParams = playerView.getLayoutParams();
-        videoFramelayoutParams = videoFrameLayout.getLayoutParams();
-        ((ViewGroup) playerView.getParent()).removeView(playerView);
-        ((ViewGroup) fullScreenIcon.getParent()).removeView(fullScreenIcon);
-        ((ViewGroup) videoFrameLayout.getParent()).removeView(videoFrameLayout);
-        fullScreenDialog.addContentView(playerView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        handle.switchToFullScreen(true);
+
+        // clear views from container
+        videoFrameLayout.removeView(playerView);
+        videoFrameLayout.removeView(fullScreenIcon);
+
+        if (fullScreenDialog == null) {
+            // create full screen dialog and show
+            fullScreenDialog = new Dialog(this.context, android.R.style.Theme_Black_NoTitleBar_Fullscreen) {
+                public void onBackPressed() {
+                    if (exoPlayerFullscreen) {
+                        closeFullscreenDialog();
+                    }
+                    super.onBackPressed();
+                }
+            };
+        }
+
+        ViewGroup.LayoutParams fullScreenParams = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        );
+        fullScreenDialog.addContentView(playerView, fullScreenParams);
         exoPlayerFullscreen = true;
         fullScreenDialog.show();
     }
 
     private void playMedia() {
-        playerView.requestFocus();
-        playerView.setVisibility(View.VISIBLE);
-        playerView.setPlayer(player);
-        player.setPlayWhenReady(true);
+        handle.play();
     }
 
     private void prepareMedia() {
+        handle.initPlayerView(
+                context,
+                inAppNotification.isTablet() && isTablet()
+        );
+        streamContainer();
+
+        handle.initExoplayer(
+                context,
+                inAppNotification.getMediaList().get(0).getMediaUrl()
+        );
+    }
+
+    private void streamContainer() {
+        // make video container visible
         videoFrameLayout = relativeLayout.findViewById(R.id.video_frame);
         videoFrameLayout.setVisibility(View.VISIBLE);
 
-        playerView = new StyledPlayerView(this.context);
+        // inflate full screen icon
         fullScreenIcon = new ImageView(this.context);
-        fullScreenIcon.setImageDrawable(
-                ResourcesCompat.getDrawable(this.context.getResources(), R.drawable.ct_ic_fullscreen_expand, null));
+        fullScreenIcon.setImageDrawable(ResourcesCompat.getDrawable(this.context.getResources(), R.drawable.ct_ic_fullscreen_expand, null));
         fullScreenIcon.setOnClickListener(v -> {
             if (!exoPlayerFullscreen) {
                 openFullscreenDialog();
@@ -364,74 +350,31 @@ public class CTInAppNativeInterstitialFragment extends CTInAppBaseFullNativeFrag
                 closeFullscreenDialog();
             }
         });
+
+        // icon layout params wrt tablet/phone
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
         if (inAppNotification.isTablet() && isTablet()) {
-
-            int playerWidth = (int) TypedValue
-                    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 408, getResources().getDisplayMetrics());
-            int playerHeight = (int) TypedValue
-                    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 229, getResources().getDisplayMetrics());
-
-            playerView.setLayoutParams(new FrameLayout.LayoutParams(playerWidth, playerHeight));
-            int iconWidth = (int) TypedValue
-                    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 30, getResources().getDisplayMetrics());
-            int iconHeight = (int) TypedValue
-                    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 30, getResources().getDisplayMetrics());
+            int iconWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 30, displayMetrics);
+            int iconHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 30, displayMetrics);
             FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(iconWidth, iconHeight);
             layoutParams.gravity = Gravity.END;
-            int iconTop = (int) TypedValue
-                    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources().getDisplayMetrics());
-            int iconRight = (int) TypedValue
-                    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, getResources().getDisplayMetrics());
+            int iconTop = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, displayMetrics);
+            int iconRight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, displayMetrics);
             layoutParams.setMargins(0, iconTop, iconRight, 0);
             fullScreenIcon.setLayoutParams(layoutParams);
         } else {
-            int width = (int) TypedValue
-                    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 240, getResources().getDisplayMetrics());
-            int height = (int) TypedValue
-                    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 134, getResources().getDisplayMetrics());
-
-            playerView.setLayoutParams(new FrameLayout.LayoutParams(width, height));
-            int iconWidth = (int) TypedValue
-                    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, getResources().getDisplayMetrics());
-            int iconHeight = (int) TypedValue
-                    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, getResources().getDisplayMetrics());
+            int iconWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, displayMetrics);
+            int iconHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, displayMetrics);
             FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(iconWidth, iconHeight);
             layoutParams.gravity = Gravity.END;
-            int iconTop = (int) TypedValue
-                    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources().getDisplayMetrics());
-            int iconRight = (int) TypedValue
-                    .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, getResources().getDisplayMetrics());
+            int iconTop = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, displayMetrics);
+            int iconRight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2, displayMetrics);
             layoutParams.setMargins(0, iconTop, iconRight, 0);
             fullScreenIcon.setLayoutParams(layoutParams);
         }
-        playerView.setShowBuffering(StyledPlayerView.SHOW_BUFFERING_WHEN_PLAYING);
-        playerView.setUseArtwork(true);
-        playerView.setControllerAutoShow(false);
-        videoFrameLayout.addView(playerView);
-        videoFrameLayout.addView(fullScreenIcon);
-        Drawable artwork = ResourcesCompat.getDrawable(this.context.getResources(), R.drawable.ct_audio, null);
-        playerView.setDefaultArtwork(artwork);
 
-        // 1. Create a default TrackSelector
-        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter.Builder(this.context).build();
-        ExoTrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory();
-        TrackSelector trackSelector = new DefaultTrackSelector(this.context,
-                videoTrackSelectionFactory);
-        // 2. Create the player
-        player = new ExoPlayer.Builder(this.context).setTrackSelector(trackSelector).build();
-        // 3. Produces DataSource instances through which media data is loaded.
-        Context ctx = this.context;
-        String userAgent = Util.getUserAgent(ctx,ctx.getPackageName());
-        String url = inAppNotification.getMediaList().get(0).getMediaUrl();
-        TransferListener listener = bandwidthMeter.getTransferListener();
-        DefaultHttpDataSource.Factory  dsf = new DefaultHttpDataSource.Factory().setUserAgent(userAgent).setTransferListener(listener);
-        DataSource.Factory dataSourceFactory = new DefaultDataSource.Factory(ctx,dsf);
-        MediaItem mediaItem = MediaItem.fromUri(url);
-        HlsMediaSource hlsMediaSource = new HlsMediaSource.Factory(dataSourceFactory).createMediaSource(mediaItem);
-        player.setMediaSource(hlsMediaSource);
-        // 4. Prepare the player with the source.
-        player.prepare();
-        player.setRepeatMode(Player.REPEAT_MODE_ONE);
-        player.seekTo(mediaPosition);
+        // add views to video container
+        videoFrameLayout.addView(handle.player());
+        videoFrameLayout.addView(fullScreenIcon);
     }
 }
---
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/video/ExoplayerHandle.kt
@@ -0,0 +1,139 @@
+package com.clevertap.android.sdk.video
+
+import android.content.Context
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.core.content.res.ResourcesCompat
+import com.clevertap.android.sdk.R
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.ExoTrackSelection
+import com.google.android.exoplayer2.trackselection.TrackSelector
+import com.google.android.exoplayer2.ui.StyledPlayerView
+import com.google.android.exoplayer2.upstream.BandwidthMeter
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.google.android.exoplayer2.util.Util
+
+class ExoplayerHandle {
+
+    companion object {
+        var mediaPosition: Long = 0
+    }
+
+    private var player: ExoPlayer? = null
+    private var playerView: StyledPlayerView? = null
+
+    private var playerViewLayoutParams: ViewGroup.LayoutParams? = null
+
+    fun initExoplayer(
+        context: Context,
+        url: String
+    ) {
+        if (player != null) {
+            return
+        }
+        val bandwidthMeter: BandwidthMeter = DefaultBandwidthMeter.Builder(context).build()
+        val videoTrackSelectionFactory: ExoTrackSelection.Factory = AdaptiveTrackSelection.Factory()
+        val trackSelector: TrackSelector = DefaultTrackSelector(
+            context,
+            videoTrackSelectionFactory
+        )
+
+        val userAgent = Util.getUserAgent(context, context.packageName)
+        val listener = bandwidthMeter.transferListener
+        val dsf = DefaultHttpDataSource.Factory().setUserAgent(userAgent).setTransferListener(listener)
+        val dataSourceFactory: DataSource.Factory = DefaultDataSource.Factory(context, dsf)
+        val mediaItem = MediaItem.fromUri(url)
+        val hlsMediaSource = HlsMediaSource.Factory(dataSourceFactory).createMediaSource(mediaItem)
+
+        player = ExoPlayer.Builder(context).setTrackSelector(trackSelector).build().apply {
+            setMediaSource(hlsMediaSource)
+            prepare()
+            setRepeatMode(Player.REPEAT_MODE_ONE)
+            seekTo(mediaPosition)
+        }
+    }
+
+    fun initPlayerView(
+        context: Context,
+        isTablet: Boolean
+    ) {
+        if (playerView != null) {
+            return
+        }
+        playerView = StyledPlayerView(context)
+
+        val playerWidth = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            if (isTablet) {
+                408f
+            } else {
+                240f
+            },
+            context.resources.displayMetrics
+        ).toInt()
+
+        val playerHeight = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            if (isTablet) {
+                299f
+            } else {
+                134f
+            },
+            context.resources.displayMetrics
+        ).toInt()
+
+        playerViewLayoutParams = FrameLayout.LayoutParams(playerWidth, playerHeight)
+        playerView!!.setLayoutParams(playerViewLayoutParams)
+        playerView!!.setShowBuffering(StyledPlayerView.SHOW_BUFFERING_WHEN_PLAYING)
+        playerView!!.useArtwork = true
+        playerView!!.controllerAutoShow = false
+        playerView!!.defaultArtwork = ResourcesCompat.getDrawable(
+            context.resources,
+            R.drawable.ct_audio,
+            null
+        )
+    }
+
+    fun play() {
+        playerView!!.requestFocus()
+        playerView!!.visibility = View.VISIBLE
+        playerView!!.player = player
+        player!!.playWhenReady = true
+    }
+
+    fun pause() {
+        if (player != null) {
+            player!!.stop()
+            player!!.release()
+            player = null
+        }
+    }
+
+    fun savePosition() {
+        if (player != null) {
+            mediaPosition = player!!.currentPosition
+        }
+    }
+
+    fun switchToFullScreen(isFullScreen: Boolean) {
+        if (isFullScreen) {
+            playerViewLayoutParams = playerView!!.layoutParams
+        } else {
+            playerView!!.setLayoutParams(playerViewLayoutParams)
+        }
+    }
+
+    fun player(): View {
+        return playerView!!
+    }
+}
