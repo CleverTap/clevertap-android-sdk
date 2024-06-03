9c3bec2340baea98c75602a26cc48575bc5ed24d
CTLalit <lalitkumar@clevertap.com>
2024-06-03 18:56:57 +0530

    Feat: SDK-3780 : Media handle
    - adds media handle which uses media3 components instead of exoplayer
    - adds a different handle class which has similar func to exo handle
    - runtime can find the correct handle based on user and attach

---
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/video/inapps/Media3Handle.kt
@@ -0,0 +1,154 @@
+package com.clevertap.android.sdk.video.inapps
+
+import android.content.Context
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.core.content.res.ResourcesCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.common.util.Util
+import androidx.media3.datasource.DataSource
+import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.datasource.DefaultHttpDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.hls.HlsMediaSource
+import androidx.media3.exoplayer.trackselection.AdaptiveTrackSelection
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
+import androidx.media3.exoplayer.trackselection.ExoTrackSelection
+import androidx.media3.exoplayer.trackselection.TrackSelector
+import androidx.media3.exoplayer.upstream.BandwidthMeter
+import androidx.media3.exoplayer.upstream.DefaultBandwidthMeter
+import androidx.media3.ui.PlayerView
+import com.clevertap.android.sdk.R
+
+class Media3Handle {
+    private var player: ExoPlayer? = null
+    private var playerView: PlayerView? = null
+
+    private var playerViewLayoutParams: ViewGroup.LayoutParams? = null
+
+    private var mediaPosition = 0L
+
+    fun initExoplayer(
+        context: Context,
+        url: String
+    ) {
+        if (player != null) {
+            return
+        }
+        val bandwidthMeter: BandwidthMeter = DefaultBandwidthMeter.Builder(context).build()
+        val videoTrackSelectionFactory: ExoTrackSelection.Factory = AdaptiveTrackSelection.Factory()
+        val trackSelector: TrackSelector = DefaultTrackSelector(
+            context,
+            videoTrackSelectionFactory
+        )
+
+        val userAgent = Util.getUserAgent(context, context.packageName)
+        val listener = bandwidthMeter.transferListener
+        val dsf = DefaultHttpDataSource.Factory().setUserAgent(userAgent).setTransferListener(listener)
+        val dataSourceFactory: DataSource.Factory = DefaultDataSource.Factory(context, dsf)
+        val mediaItem = MediaItem.fromUri(url)
+        val hlsMediaSource = HlsMediaSource.Factory(dataSourceFactory).createMediaSource(mediaItem)
+
+        player = ExoPlayer.Builder(context).setTrackSelector(trackSelector).build().apply {
+            setMediaSource(hlsMediaSource)
+            prepare()
+            repeatMode = Player.REPEAT_MODE_ONE
+            seekTo(mediaPosition)
+        }
+    }
+
+    fun initPlayerView(
+        context: Context,
+        isTablet: Boolean
+    ) {
+        if (playerView != null) {
+            return
+        }
+
+        val playerWidth = playerWidth(context = context, isTablet = isTablet)
+        val playerHeight = playerHeight(context = context, isTablet = isTablet)
+
+        playerView = PlayerView(context).apply {
+            playerViewLayoutParams = FrameLayout.LayoutParams(playerWidth, playerHeight)
+            setLayoutParams(playerViewLayoutParams)
+            setShowBuffering(PlayerView.SHOW_BUFFERING_WHEN_PLAYING)
+            useArtwork = true
+            controllerAutoShow = false
+            defaultArtwork = ResourcesCompat.getDrawable(
+                context.resources,
+                R.drawable.ct_audio,
+                null
+            )
+        }
+    }
+
+    fun play() {
+        playerView?.let { pv ->
+            pv.requestFocus()
+            pv.visibility = View.VISIBLE
+            pv.player = player
+        }
+        player?.let {
+            it.playWhenReady = true
+        }
+    }
+
+    fun pause() {
+        player?.let { ep ->
+            ep.stop()
+            ep.release()
+            player = null
+        }
+    }
+
+    fun savePosition() {
+        if (player != null) {
+            mediaPosition = player!!.currentPosition
+        }
+    }
+
+    fun switchToFullScreen(isFullScreen: Boolean) {
+        if (isFullScreen) {
+            playerViewLayoutParams = playerView!!.layoutParams
+        } else {
+            playerView!!.setLayoutParams(playerViewLayoutParams)
+        }
+    }
+
+    fun videoSurface(): View {
+        return playerView!!
+    }
+
+    private fun playerWidth(
+        context: Context,
+        isTablet: Boolean
+    ): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            if (isTablet) {
+                408f
+            } else {
+                240f
+            },
+            context.resources.displayMetrics
+        ).toInt()
+    }
+
+    private fun playerHeight(
+        context: Context,
+        isTablet: Boolean
+    ): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            if (isTablet) {
+                299f
+            } else {
+                134f
+            },
+            context.resources.displayMetrics
+        ).toInt()
+    }
+}
---
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/video/inbox/Media3Handle.kt
@@ -0,0 +1,161 @@
+package com.clevertap.android.sdk.video.inbox
+
+import android.content.Context
+import android.content.res.Configuration
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.view.View
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.common.util.Util
+import androidx.media3.datasource.DataSource
+import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.datasource.DefaultHttpDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.hls.HlsMediaSource
+import androidx.media3.exoplayer.trackselection.AdaptiveTrackSelection
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
+import androidx.media3.exoplayer.trackselection.ExoTrackSelection
+import androidx.media3.exoplayer.trackselection.TrackSelector
+import androidx.media3.exoplayer.upstream.BandwidthMeter
+import androidx.media3.exoplayer.upstream.DefaultBandwidthMeter
+import androidx.media3.ui.AspectRatioFrameLayout
+import androidx.media3.ui.PlayerView
+
+class Media3Handle {
+
+    private var videoSurfaceView: PlayerView? = null
+    private var player: ExoPlayer? = null
+
+    fun initExoplayer(
+        context: Context,
+        buffering: () -> Void,
+        playerReady: () -> Void,
+    ) {
+        if (player != null) {
+            return
+        }
+
+        val videoTrackSelectionFactory: ExoTrackSelection.Factory = AdaptiveTrackSelection.Factory()
+        val trackSelector: TrackSelector = DefaultTrackSelector(context, videoTrackSelectionFactory)
+
+        player = ExoPlayer.Builder(context)
+            .setTrackSelector(trackSelector)
+            .build()
+            .apply {
+                volume = 0f // start off muted
+                addListener(object : Player.Listener {
+                    override fun onPlaybackStateChanged(playbackState: Int) {
+                        when (playbackState) {
+                            Player.STATE_BUFFERING -> {
+                                buffering.invoke()
+                            }
+
+                            Player.STATE_ENDED -> if (player != null) {
+                                seekTo(0)
+                                playWhenReady = false
+                                videoSurfaceView?.showController()
+                            }
+
+                            Player.STATE_READY -> {
+                                playerReady.invoke()
+                            }
+
+                            Player.STATE_IDLE -> {}
+                            else -> {}
+                        }
+                    }
+                })
+            }
+    }
+
+    fun videoSurface(): View {
+        return videoSurfaceView!!
+    }
+
+    fun setPlayWhenReady(play: Boolean) {
+        player?.let { ep ->
+            ep.playWhenReady = play
+        }
+    }
+
+    fun pause() {
+        player?.let { ep ->
+            ep.stop()
+            ep.release()
+        }
+        player = null
+        videoSurfaceView = null
+    }
+
+    fun initPlayerView(
+        context: Context,
+        artworkAsset: () -> Drawable,
+    ) {
+        if (videoSurfaceView != null) {
+            return
+        }
+        videoSurfaceView = PlayerView(context).apply {
+            setBackgroundColor(Color.TRANSPARENT)
+
+            resizeMode = if (context.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                AspectRatioFrameLayout.RESIZE_MODE_FILL
+            } else {
+                AspectRatioFrameLayout.RESIZE_MODE_FIT
+            }
+            useArtwork = true
+            defaultArtwork = artworkAsset.invoke()
+
+            useController = true
+            controllerAutoShow = false
+            player = this@Media3Handle.player
+        }
+    }
+
+    fun playerVolume(): Float = player?.let { ep -> ep.volume } ?: 0f
+
+    fun handleMute() {
+        player?.let { ep ->
+            val playerVolume = playerVolume()
+
+            if (playerVolume > 0) {
+                ep.volume = 0f
+            } else if (playerVolume == 0f) {
+                ep.volume = 1f
+            }
+        }
+    }
+
+    fun startPlaying(
+        ctx: Context,
+        uriString: String,
+        isMediaAudio: Boolean,
+        isMediaVideo: Boolean,
+    ) {
+        videoSurfaceView?.let { spv ->
+            spv.requestFocus()
+            spv.setShowBuffering(PlayerView.SHOW_BUFFERING_NEVER)
+        }
+        // Prepare the player with the source.
+        player?.let { ep ->
+            val defaultBandwidthMeter = DefaultBandwidthMeter.Builder(ctx).build()
+            val userAgent = Util.getUserAgent(ctx, ctx.packageName)
+            val mediaItem: MediaItem = MediaItem.fromUri(uriString)
+            val dsf = DefaultHttpDataSource.Factory().setUserAgent(userAgent)
+                .setTransferListener(defaultBandwidthMeter)
+            val dataSourceFactory: DataSource.Factory = DefaultDataSource.Factory(ctx, dsf)
+            val hlsMediaSource = HlsMediaSource.Factory(dataSourceFactory).createMediaSource(mediaItem)
+
+            ep.setMediaSource(hlsMediaSource)
+            ep.prepare()
+            if (isMediaAudio) {
+                videoSurfaceView?.showController() //show controller for audio as it is not autoplay
+                ep.playWhenReady = false
+                ep.volume = 1f
+            } else if (isMediaVideo) {
+                ep.playWhenReady = true
+                ep.volume = playerVolume()
+            }
+        }
+    }
+}
