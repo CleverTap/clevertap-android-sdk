49f583b5f6f025a8ca3d1f0a70a612b3166f0822
piyush.kukadiya <piyush.kukadiya@clevertap.com>
2024-06-07 17:00:01 +0530

    feat(file_type_download_support-v2) : rename isFileExistsForUrl() to doesFileExistForUrl() SDK-3872

--- clevertap-core/src/main/java/com/clevertap/android/sdk/CleverTapAPI.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/CleverTapAPI.java
@@ -3567,7 +3567,7 @@ public class CleverTapAPI implements CTInboxActivity.InboxActivityListener {
      * @param url the non-null url for which to check the existence of the file.
      * @return true if a file exists for the specified url, false otherwise.
      */
-    public boolean isFileExistsForUrl(@NonNull String url){
+    public boolean doesFileExistForUrl(@NonNull String url){
         Logger logger = coreState.getConfig().getLogger();
         InAppResourceProvider inAppResourceProvider = new InAppResourceProvider(context, logger);
         return inAppResourceProvider.isFileCached(url);
