dc8e707888c6d77940c2944481c2bb2c4ccfcb9a
CTLalit <lalitkumar@clevertap.com>
2024-09-03 09:58:15 +0530

    feat(SDK-4030): Optimises service fetch and computations
    - reduces service fetches from context
    - reduces version checks
    - refactors code to add placeholder class
    - returns window result as whole

--- clevertap-core/src/main/java/com/clevertap/android/sdk/DeviceInfo.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/DeviceInfo.java
@@ -24,6 +24,7 @@ import android.view.WindowInsets;
 import android.view.WindowManager;
 import android.view.WindowMetrics;
 import androidx.annotation.IntDef;
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.annotation.RestrictTo;
 import androidx.annotation.RestrictTo.Scope;
@@ -83,7 +84,7 @@ public class DeviceInfo {
 
         private final double width;
 
-        private String appBucket;
+        private final String appBucket;
 
         private int localInAppCount;
 
@@ -101,14 +102,72 @@ public class DeviceInfo {
             bluetoothVersion = getBluetoothVersion();
             countryCode = getCountryCode();
             sdkVersion = getSdkVersion();
-            height = getHeight();
-            width = getWidth();
-            dpi = getDPI();
+
+            WindowSize result = getWindowSizeData();
+
+            width = toTwoPlaces(result.doubleWidth);
+            height = toTwoPlaces(result.doubleHeight);
+            dpi = result.localDpi;
+
+
             localInAppCount = getLocalInAppCountFromPreference();
             locale = getDeviceLocale();
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
                 appBucket = getAppBucket();
+            } else {
+                appBucket = null;
             }
+        }
+
+        @NonNull
+        private WindowSize getWindowSizeData() {
+
+            WindowManager wm = getWindowManager();
+
+            if (wm == null) {
+                return new WindowSize(0, 0, 0);
+            }
+
+            int localWidth;
+            int localHeight;
+            float localXDpi;
+            float localYDpi;
+            int localDpi;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                WindowMetrics windowMetrics = wm.getCurrentWindowMetrics();
+                Configuration configuration = context.getResources().getConfiguration();
+                Insets insets = windowMetrics.getWindowInsets()
+                        .getInsetsIgnoringVisibility(WindowInsets.Type.systemGestures());
+
+                localWidth = windowMetrics.getBounds().width() -
+                        insets.right - insets.left;
+                localHeight = windowMetrics.getBounds().height() -
+                        insets.top - insets.bottom;
+
+                localXDpi = configuration.densityDpi;
+                localYDpi = configuration.densityDpi;
+                localDpi = configuration.densityDpi;
+
+            } else {
+                DisplayMetrics dm = new DisplayMetrics();
+                wm.getDefaultDisplay().getMetrics(dm);
+
+                localWidth = dm.widthPixels;
+                localHeight = dm.heightPixels;
+                localXDpi = dm.xdpi;
+                localYDpi = dm.ydpi;
+                localDpi = dm.densityDpi;
+            }
+
+            // legacy code -> conversion to double is copied
+            double doubleWidth = localWidth / localXDpi;
+            double doubleHeight = localHeight / localYDpi;
+            return new WindowSize(
+                    localDpi,
+                    toTwoPlaces(doubleWidth),
+                    toTwoPlaces(doubleHeight)
+            );
         }
 
         private String getBluetoothVersion() {
@@ -158,55 +217,6 @@ public class DeviceInfo {
                 return "";
             }
             return "";
-        }
-
-        private int getDPI() {
-            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-            if (wm == null) {
-                return 0;
-            }
-            //Returns the dpi using Device Configuration API for API30 above
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                Configuration configuration = context.getResources().getConfiguration();
-                return configuration.densityDpi;
-            } else {
-                DisplayMetrics dm = new DisplayMetrics();
-                wm.getDefaultDisplay().getMetrics(dm);
-                return dm.densityDpi;
-            }
-        }
-
-        private double getHeight() {
-            int height;
-            float dpi;
-
-            WindowManager wm = getWindowManager();
-            if (wm == null) {
-                return 0.0;
-            }
-
-            //Returns height using WindowMetrics API for API30 above
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                WindowMetrics windowMetrics = wm.getCurrentWindowMetrics();
-                Configuration configuration = context.getResources().getConfiguration();
-                Insets insets = windowMetrics.getWindowInsets()
-                        .getInsetsIgnoringVisibility(WindowInsets.Type.systemGestures());
-
-                height = windowMetrics.getBounds().height() -
-                        insets.top - insets.bottom;
-
-                dpi = configuration.densityDpi;
-
-            } else {
-                DisplayMetrics dm = new DisplayMetrics();
-                wm.getDefaultDisplay().getMetrics(dm);
-
-                height = dm.heightPixels;
-                dpi = dm.ydpi;
-            }
-            // Calculate the width in inches
-            double rHeight = height / dpi;
-            return toTwoPlaces(rHeight);
         }
 
         private String getManufacturer() {
@@ -278,39 +288,6 @@ public class DeviceInfo {
             return null;
         }
 
-        private double getWidth() {
-            int width;
-            float dpi;
-
-            WindowManager wm = getWindowManager();
-            if (wm == null) {
-                return 0.0;
-            }
-
-            //Returns width using WindowMetrics API for API30 above
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                WindowMetrics windowMetrics = wm.getCurrentWindowMetrics();
-                Configuration configuration = context.getResources().getConfiguration();
-                Insets insets = windowMetrics.getWindowInsets()
-                        .getInsetsIgnoringVisibility(WindowInsets.Type.systemGestures());
-                width = windowMetrics.getBounds().width() -
-                        insets.right - insets.left;
-
-                dpi = configuration.densityDpi;
-
-            } else {
-                DisplayMetrics dm = new DisplayMetrics();
-                wm.getDefaultDisplay().getMetrics(dm);
-
-                width = dm.widthPixels;
-                dpi = dm.xdpi;
-            }
-            // Calculate the width in inches
-            double rWidth = width / dpi;
-            return toTwoPlaces(rWidth);
-
-        }
-
         private String getDeviceLocale() {
             String language = Locale.getDefault().getLanguage();
             if ("".equals(language)) {
@@ -328,6 +305,21 @@ public class DeviceInfo {
             result = Math.round(result);
             result = result / 100;
             return result;
+        }
+    }
+
+    /**
+     * Placeholder for data
+     */
+    private static class WindowSize {
+        public final int localDpi;
+        public final double doubleWidth;
+        public final double doubleHeight;
+
+        public WindowSize(int localDpi, double doubleWidth, double doubleHeight) {
+            this.localDpi = localDpi;
+            this.doubleWidth = doubleWidth;
+            this.doubleHeight = doubleHeight;
         }
     }
 
