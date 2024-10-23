f179cc2cfb2b291574625df88da83bc22f06ef39
piyush-kukadiya <piyush.kukadiya@clevertap.com>
2024-10-24 00:03:24 +0530

    feat(multi_triggers) : add UserEventLog DAO interface MC-2083

---
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/userEventLogs/UserEventLogDAO.kt
@@ -0,0 +1,32 @@
+package com.clevertap.android.sdk.userEventLogs
+
+import androidx.annotation.WorkerThread
+
+interface UserEventLogDAO {
+
+    // Insert a new event by deviceID
+    @WorkerThread
+    fun insertEventByDeviceID(deviceID: String, eventName: String): Long
+
+    // Update an event by deviceID
+    @WorkerThread
+    fun updateEventByDeviceID(deviceID: String, eventName: String): Boolean
+
+    // Read an event by deviceID
+    @WorkerThread
+    fun readEventByDeviceID(deviceID: String, eventName: String): UserEventLog?
+
+    // Check if an event exists by deviceID
+    @WorkerThread
+    fun eventExistsByDeviceID(deviceID: String, eventName: String): Boolean
+
+    // Get all events for a particular deviceID
+    @WorkerThread
+    fun allEventsByDeviceID(deviceID: String): List<UserEventLog>
+
+    // Get all events
+    @WorkerThread
+    fun allEvents(): List<UserEventLog>
+    @WorkerThread
+    fun cleanUpExtraEvents(threshold: Int): Boolean
+}
