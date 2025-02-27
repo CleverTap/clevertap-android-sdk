6d69bfe796b39f8cb308bfdbab2f7375022fa19e
CTLalit <lalitkumar@clevertap.com>
2025-02-27 21:16:11 +0530

    feat(SDK-4449) fixes hms sdk compilation problems
    - moves pushtype creation within hms itself
    - this can be plugged in via new sdk method in future

--- clevertap-hms/src/main/java/com/clevertap/android/hms/HmsConstants.java
+++ clevertap-hms/src/main/java/com/clevertap/android/hms/HmsConstants.java
@@ -1,12 +1,14 @@
 package com.clevertap.android.hms;
 
-import com.clevertap.android.sdk.pushnotification.PushType;
-
 interface HmsConstants {
 
-    String HMS_LOG_TAG = PushType.HPS.toString();
+    String HMS_DELIVERY_TYPE = "hps";
+    String CT_HUAWEI_PROVIDER_CLASS = "com.clevertap.android.hms.HmsPushProvider";
+    String HUAWEI_SDK_CLASS = "com.huawei.hms.push.HmsMessageService";
+    String HPS_PROPERTY_REG_ID = "hps_token";
+
+    String HMS_LOG_TAG = "HPS";
     String HCM_SCOPE = "HCM";
     String APP_ID_KEY = "client/app_id";
     int MIN_CT_ANDROID_SDK_VERSION = 30800;
-
 }
