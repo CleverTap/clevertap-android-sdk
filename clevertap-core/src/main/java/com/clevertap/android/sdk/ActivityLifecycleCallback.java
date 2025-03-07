90a51f6423bbacf452beef06bf9f08e542f3b18e
CTLalit <lalitkumar@clevertap.com>
2025-03-07 13:47:23 +0530

    feat(SDK-4481) adds methods for utility
    - adds changes to ActivityLifecycleCallback.register method to append providers for those who want to install it
    - adds new CleverTapAPI.getDefaultInstance method with overload for appending push providers

--- clevertap-core/src/main/java/com/clevertap/android/sdk/ActivityLifecycleCallback.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/ActivityLifecycleCallback.java
@@ -1,10 +1,14 @@
 package com.clevertap.android.sdk;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.Application;
-import android.os.Build;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import com.clevertap.android.sdk.pushnotification.PushType;
+
+import java.util.List;
 
 /**
  * Class for handling activity lifecycle events
@@ -14,45 +18,42 @@ public final class ActivityLifecycleCallback {
 
     public static boolean registered = false;
     private static String cleverTapId = null;
+    private static List<PushType> pushTypes = null;
     private static final Application.ActivityLifecycleCallbacks lifecycleCallbacks = new Application.ActivityLifecycleCallbacks() {
 
         @Override
-        public void onActivityCreated(Activity activity, Bundle bundle) {
-            if (cleverTapId != null) {
-                CleverTapAPI.onActivityCreated(activity, cleverTapId);
-            } else {
-                CleverTapAPI.onActivityCreated(activity);
-            }
+        public void onActivityCreated(@NonNull Activity activity, Bundle bundle) {
+            CleverTapAPI.onActivityCreated(activity, cleverTapId, pushTypes);
         }
 
         @Override
-        public void onActivityDestroyed(Activity activity) {
+        public void onActivityDestroyed(@NonNull Activity activity) {
         }
 
         @Override
-        public void onActivityPaused(Activity activity) {
+        public void onActivityPaused(@NonNull Activity activity) {
             CleverTapAPI.onActivityPaused();
         }
 
         @Override
-        public void onActivityResumed(Activity activity) {
+        public void onActivityResumed(@NonNull Activity activity) {
             if (cleverTapId != null) {
-                CleverTapAPI.onActivityResumed(activity, cleverTapId);
+                CleverTapAPI.onActivityResumed(activity, cleverTapId, pushTypes);
             } else {
                 CleverTapAPI.onActivityResumed(activity);
             }
         }
 
         @Override
-        public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+        public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {
         }
 
         @Override
-        public void onActivityStarted(Activity activity) {
+        public void onActivityStarted(@NonNull Activity activity) {
         }
 
         @Override
-        public void onActivityStopped(Activity activity) {
+        public void onActivityStopped(@NonNull Activity activity) {
         }
     };
 
@@ -60,9 +61,17 @@ public final class ActivityLifecycleCallback {
      * Enables lifecycle callbacks for Android devices
      *
      * @param application App's Application object
+     */
+    public static void register(android.app.Application application) {
+        register(application, null);
+    }
+
+    /**
+     * Enables lifecycle callbacks for Android devices
+     *
+     * @param application App's Application object
      * @param cleverTapID Custom CleverTap ID
      */
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     public static void register(android.app.Application application, final String cleverTapID) {
         if (application == null) {
             Logger.i("Application instance is null/system API is too old");
@@ -82,13 +91,27 @@ public final class ActivityLifecycleCallback {
         Logger.i("Activity Lifecycle Callback successfully registered");
     }
 
-    /**
-     * Enables lifecycle callbacks for Android devices
-     *
-     * @param application App's Application object
-     */
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-    public static void register(android.app.Application application) {
-        register(application, null);
+    public static void register(
+            android.app.Application application,
+            final String cleverTapID,
+            final List<PushType> pushTypes
+    ) {
+        if (application == null) {
+            Logger.i("Application instance is null/system API is too old");
+            return;
+        }
+
+        if (registered) {
+            Logger.v("Lifecycle callbacks have already been registered");
+            return;
+        }
+
+        ActivityLifecycleCallback.cleverTapId = cleverTapID;
+        ActivityLifecycleCallback.pushTypes = pushTypes;
+        ActivityLifecycleCallback.registered = true;
+
+        application.unregisterActivityLifecycleCallbacks(lifecycleCallbacks);
+        application.registerActivityLifecycleCallbacks(lifecycleCallbacks);
+        Logger.i("Activity Lifecycle Callback successfully registered");
     }
 }
