7eaa47d15c0aceed979bbe4ec6600b5ff9bb5099
CTLalit <lalitkumar@clevertap.com>
2025-03-10 17:49:45 +0530

    feat(SDK-4481) converts interface for constants to a class

--- clevertap-core/src/main/java/com/clevertap/android/sdk/pushnotification/PushConstants.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/pushnotification/PushConstants.java
@@ -2,20 +2,24 @@ package com.clevertap.android.sdk.pushnotification;
 
 import androidx.annotation.NonNull;
 
-public interface PushConstants {
+public class PushConstants {
 
-    String LOG_TAG = "PushProvider";
+    private PushConstants() {
+        // intentional
+    }
 
-    String FCM_LOG_TAG = "FCM";
+    public static final String LOG_TAG = "PushProvider";
+
+    public static final String FCM_LOG_TAG = "FCM";
 
     @NonNull
-    String FCM_DELIVERY_TYPE = "fcm";
+    public static final String FCM_DELIVERY_TYPE = "fcm";
     @NonNull
-    String CT_FIREBASE_PROVIDER_CLASS = "com.clevertap.android.sdk.pushnotification.fcm.FcmPushProvider";
-    String FIREBASE_SDK_CLASS = "com.google.firebase.messaging.FirebaseMessagingService";
-    String FCM_PROPERTY_REG_ID = "fcm_token";
+    public static final String CT_FIREBASE_PROVIDER_CLASS = "com.clevertap.android.sdk.pushnotification.fcm.FcmPushProvider";
+    public static final String FIREBASE_SDK_CLASS = "com.google.firebase.messaging.FirebaseMessagingService";
+    public static final String FCM_PROPERTY_REG_ID = "fcm_token";
 
-    PushType FCM = new PushType(
+    public static final PushType FCM = new PushType(
             FCM_DELIVERY_TYPE,
             FCM_PROPERTY_REG_ID,
             CT_FIREBASE_PROVIDER_CLASS,
