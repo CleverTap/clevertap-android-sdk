2f67a4041225eecef98a4c2686cb0f50c149d7ff
CTLalit <lalitkumar@clevertap.com>
2025-03-10 19:18:08 +0530

    feat(SDK-4481) fixes the PushNotificationHandler.java
    - fixes the onMessageReceived callback inputs
    - fcm and hms handlers call this method with correct constants.

--- clevertap-core/src/main/java/com/clevertap/android/sdk/pushnotification/PushConstants.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/pushnotification/PushConstants.java
@@ -18,6 +18,7 @@ public class PushConstants {
     public static final String CT_FIREBASE_PROVIDER_CLASS = "com.clevertap.android.sdk.pushnotification.fcm.FcmPushProvider";
     public static final String FIREBASE_SDK_CLASS = "com.google.firebase.messaging.FirebaseMessagingService";
     public static final String FCM_PROPERTY_REG_ID = "fcm_token";
+    public static final String FCM_PUSH_TYPE = "FCM";
 
     public static final PushType FCM = new PushType(
             FCM_DELIVERY_TYPE,
