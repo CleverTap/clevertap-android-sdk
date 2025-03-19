5ec8e8681a6e3363e13fdbda84a64e5c85e2dbfd
Vassil Angelov <vassil@clevertap.com>
2025-03-19 12:39:07 +0200

    Add app rating system template
    Improve testing of system templates

--- clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/customtemplates/system/SystemTemplates.kt
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/inapp/customtemplates/system/SystemTemplates.kt
@@ -1,15 +1,16 @@
 package com.clevertap.android.sdk.inapp.customtemplates.system
 
-import android.content.Context
 import com.clevertap.android.sdk.inapp.InAppActionHandler
 import com.clevertap.android.sdk.inapp.customtemplates.CustomTemplate
 
 internal object SystemTemplates {
 
     fun getSystemTemplates(
-        systemActionHandler: InAppActionHandler,
-        context: Context
+        systemActionHandler: InAppActionHandler
     ): Set<CustomTemplate> {
-        return setOf(OpenUrlTemplate.createTemplate(systemActionHandler, context))
+        return setOfNotNull(
+            OpenUrlTemplate.createTemplate(systemActionHandler),
+            PlayStoreAppRatingTemplate.createTemplate(systemActionHandler)
+        )
     }
 }
--- clevertap-core/src/test/java/com/clevertap/android/sdk/inapp/InAppActionHandlerTest.kt
+++ clevertap-core/src/test/java/com/clevertap/android/sdk/inapp/InAppActionHandlerTest.kt
@@ -1,19 +1,30 @@
 package com.clevertap.android.sdk.inapp
 
 import android.content.Context
+import android.content.Intent
 import com.clevertap.android.sdk.Constants
 import com.clevertap.android.sdk.Logger
 import com.clevertap.android.shared.test.BaseTestCase
+import com.google.android.play.core.review.testing.FakeReviewManager
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class InAppActionHandlerTest : BaseTestCase() {
 
-    private val inAppActionHandler = InAppActionHandler(mockk<Logger>(relaxed = true))
+    private lateinit var inAppActionHandler: InAppActionHandler
+
+    @Before
+    fun init() {
+        inAppActionHandler = InAppActionHandler(
+            application,
+            mockk<Logger>(relaxed = true),
+            playStoreReviewManagerProvider = { FakeReviewManager(it) })
+    }
 
     @Test
     fun `openUrl should return if an activity is found to handle the url`() {
@@ -54,4 +65,39 @@ class InAppActionHandlerTest : BaseTestCase() {
                 })
         }
     }
+
+    @Test
+    fun `openUrl should add NEW_TASK Intent flag only when no launchContext is provided`() {
+        val url = "https://clevertap.com"
+        val mockContext = mockk<Context>(relaxed = true)
+        val inAppActionHandler = InAppActionHandler(
+            mockContext,
+            logger = mockk(relaxed = true)
+        )
+
+        assertTrue(inAppActionHandler.openUrl(url))
+
+        verify {
+            mockContext.startActivity(
+                match { intent ->
+                    intent.flags and Intent.FLAG_ACTIVITY_NEW_TASK != 0
+                })
+        }
+
+        val mockLaunchContext = mockk<Context>(relaxed = true)
+        assertTrue(inAppActionHandler.openUrl(url, mockLaunchContext))
+
+        verify {
+            mockLaunchContext.startActivity(
+                match { intent ->
+                    intent.flags and Intent.FLAG_ACTIVITY_NEW_TASK == 0
+                })
+        }
+    }
+
+    @Test
+    fun `isPlayStoreReviewLibraryAvailable should return true when review library is included`() {
+        assertTrue(inAppActionHandler.isPlayStoreReviewLibraryAvailable())
+    }
+
 }
