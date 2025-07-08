9c6f3ad2bf485b6fef5a61c57671d5af3af61410
anush <anush@clevertap.com>
2025-07-08 12:18:54 +0530

    task(SDK-4977) - Converts ClevertapFileProvider java file to .kt

--- clevertap-core/src/main/java/com/clevertap/android/sdk/CleverTapFileProvider.kt
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/CleverTapFileProvider.kt
@@ -1,13 +1,9 @@
-package com.clevertap.android.sdk;
+package com.clevertap.android.sdk
 
-import androidx.core.content.FileProvider;
+import androidx.core.content.FileProvider
 
 /**
  * CleverTap FileProvider to avoid manifest merger conflicts
  * when apps already have their own FileProvider.
  */
-public class CleverTapFileProvider extends FileProvider {
-    // This class intentionally left empty
-    // It simply extends FileProvider to provide a unique class name
-    // for CleverTap's file sharing needs
-}
+class CleverTapFileProvider : FileProvider()
