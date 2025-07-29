6f1486cd88c9c9711ec02e678b531e533caf5528
CTLalit <144685420+CTLalit@users.noreply.github.com>
2025-07-29 19:41:23 +0530

    task(SDK-5022): Fixes method overload usage
    - calls base method from overload (kind of overload)
    - structures similar stuff together.

--- clevertap-core/src/main/java/com/clevertap/android/sdk/StorageHelper.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/StorageHelper.java
@@ -23,10 +23,6 @@ public final class StorageHelper {
         return getPreferences(context, null);
     }
 
-    public static String getString(@NonNull Context context, @NonNull String key, String defaultValue) {
-        return getPreferences(context).getString(key, defaultValue);
-    }
-
     /**
      * returns a String From Local Storage.
      * The key used for getting the value depends upon a few factors:
@@ -42,24 +38,12 @@ public final class StorageHelper {
         return getString(context, storageKeyWithSuffix(accountId, rawKey), defaultValue);
     }
 
-    public static void persist(final SharedPreferences.Editor editor) {
-        try {
-            editor.apply();
-        } catch (Throwable t) {
-            Logger.v("CRITICAL: Failed to persist shared preferences!", t);
-        }
+    public static String getString(@NonNull Context context, @NonNull String key, String defaultValue) {
+        return getPreferences(context).getString(key, defaultValue);
     }
 
-    /**
-     * Use this method, when you are sure that you are on background thread
-     */
-    @WorkerThread
-    public static void persistImmediately(final SharedPreferences.Editor editor) {
-        try {
-            editor.commit();
-        } catch (Throwable t) {
-            Logger.v("CRITICAL: Failed to persist shared preferences!", t);
-        }
+    static String getString(Context context, String nameSpace, String key, String defaultValue) {
+        return getPreferences(context, nameSpace).getString(key, defaultValue);
     }
 
     public static void putString(Context context, String key, String value) {
@@ -69,34 +53,14 @@ public final class StorageHelper {
     }
 
     public static void putString(Context context, String accountId, String key, String value) {
-        SharedPreferences prefs = getPreferences(context);
-        SharedPreferences.Editor editor = prefs.edit().putString(storageKeyWithSuffix(accountId, key), value);
-        persist(editor);
+        String prefKey = storageKeyWithSuffix(accountId, key);
+        putString(context, prefKey, value);
     }
 
     public static void putStringImmediate(Context context, String key, String value) {
-
         SharedPreferences prefs = getPreferences(context);
         SharedPreferences.Editor editor = prefs.edit().putString(key, value);
         persistImmediately(editor);
-    }
-
-    public static void remove(Context context, String key) {
-        SharedPreferences prefs = getPreferences(context);
-        SharedPreferences.Editor editor = prefs.edit().remove(key);
-        persist(editor);
-    }
-
-    public static void removeImmediate(Context context, String key) {
-        SharedPreferences prefs = getPreferences(context);
-        SharedPreferences.Editor editor = prefs.edit().remove(key);
-        persistImmediately(editor);
-    }
-
-    //Preferences
-
-    public static String storageKeyWithSuffix(String accountID, @NonNull String key) {
-        return key + ":" + accountID;
     }
 
     @SuppressWarnings("SameParameterValue")
@@ -114,7 +78,8 @@ public final class StorageHelper {
 
     @SuppressWarnings("SameParameterValue")
     public static int getIntFromPrefs(Context context, String accountId, String rawKey, int defaultValue) {
-        return getInt(context, storageKeyWithSuffix(accountId, rawKey), defaultValue);
+        String key = storageKeyWithSuffix(accountId, rawKey);
+        return getInt(context, key, defaultValue);
     }
 
     static long getLong(Context context, String key, long defaultValue) {
@@ -133,11 +98,8 @@ public final class StorageHelper {
             int defaultValue,
             String nameSpace
     ) {
-        return getLong(context, nameSpace, storageKeyWithSuffix(accountId, rawKey), defaultValue);
-    }
-
-    static String getString(Context context, String nameSpace, String key, String defaultValue) {
-        return getPreferences(context, nameSpace).getString(key, defaultValue);
+        String key = storageKeyWithSuffix(accountId, rawKey);
+        return getLong(context, nameSpace, key, defaultValue);
     }
 
     public static void putBoolean(Context context, String key, boolean value) {
@@ -168,5 +130,41 @@ public final class StorageHelper {
         SharedPreferences prefs = getPreferences(context);
         SharedPreferences.Editor editor = prefs.edit().putLong(key, value);
         persist(editor);
+    }
+
+    public static void remove(Context context, String key) {
+        SharedPreferences prefs = getPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit().remove(key);
+        persist(editor);
+    }
+
+    public static void removeImmediate(Context context, String key) {
+        SharedPreferences prefs = getPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit().remove(key);
+        persistImmediately(editor);
+    }
+
+    public static void persist(final SharedPreferences.Editor editor) {
+        try {
+            editor.apply();
+        } catch (Throwable t) {
+            Logger.v("CRITICAL: Failed to persist shared preferences!", t);
+        }
+    }
+
+    /**
+     * Use this method, when you are sure that you are on background thread
+     */
+    @WorkerThread
+    public static void persistImmediately(final SharedPreferences.Editor editor) {
+        try {
+            editor.commit();
+        } catch (Throwable t) {
+            Logger.v("CRITICAL: Failed to persist shared preferences!", t);
+        }
+    }
+
+    public static String storageKeyWithSuffix(String accountID, @NonNull String key) {
+        return key + ":" + accountID;
     }
 }
