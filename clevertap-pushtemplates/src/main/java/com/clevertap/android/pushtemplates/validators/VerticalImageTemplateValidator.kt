e730050f602baefe6aa68e9ef181458d791b44ed
Anush-Shand <127097095+Anush-Shand@users.noreply.github.com>
2026-03-30 20:01:36 +0530

    task(SDK-5545) - Vertical Template Implementation (#965)
    * task(SDK-5545) - Vertical Template Implementation
    
    * task(SDK-5618) - Fixes button layout_width
    
    * task(SDK-5545) - Improves layout
    
    * task(SDK-5545) - Default Alt Text Fix
    
    * task(SDK-5545) - Adds tests for Vertical Template
    
    * task(SDK-5545) - Adds default colours
    
    * task(SDK-5545) - Rounded corners by default
    
    * task(SDK-5545) - Adds pt_ prefix
    
    * task(SDK-5545) - Code reuse
    
    * task(SDK-5545) - Improves signature of VerticalImageButtonData
    
    * task(SDK-5545) - Adds log for early return
    
    * task(SDK-5545) - Accepts gradient direction in degrees for linear gradient
    
    * task(SDK-5545) - Allows null deeplink to navigate to app by default

---
+++ clevertap-pushtemplates/src/main/java/com/clevertap/android/pushtemplates/validators/VerticalImageTemplateValidator.kt
@@ -0,0 +1,14 @@
+package com.clevertap.android.pushtemplates.validators
+
+import com.clevertap.android.pushtemplates.checkers.Checker
+
+class VerticalImageTemplateValidator(private var validator: Validator) : TemplateValidator(validator.keys) {
+
+    override fun validate(): Boolean {
+        return validator.validate() && super.validateKeys()
+    }
+
+    override fun loadKeys(): List<Checker<out Any>> {
+        return listOf(keys[PT_BIG_IMG]!!)
+    }
+}
