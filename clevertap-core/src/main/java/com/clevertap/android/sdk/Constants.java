8842044458a5f7a9c16a9c2421197fa232b20036
deeksha <deeksha@clevertap.com>
2026-07-09 16:05:37 +0530

    fix(accessibility): increase inapp close button touch target to 48dp and add accessibility label
    - Increase INAPP_CLOSE_IV_WIDTH from 40dp to 48dp per IS 17802 minimum touch target
    - Set contentDescription and importantForAccessibility in CloseImageView constructors
    - Update all 6 inapp layouts: cover, cover_image, half_interstitial,
      half_interstitial_image, interstitial, interstitial_image (30dp -> 48dp)

--- clevertap-core/src/main/java/com/clevertap/android/sdk/Constants.java
+++ clevertap-core/src/main/java/com/clevertap/android/sdk/Constants.java
@@ -118,7 +118,7 @@ public interface Constants {
     String INAPP_KEY = "inApp";
     String INAPP_WZRK_PIVOT = "wzrk_pivot";
     String INAPP_WZRK_CGID = "wzrk_cgId";
-    int INAPP_CLOSE_IV_WIDTH = 40;
+    int INAPP_CLOSE_IV_WIDTH = 48;
     String INAPP_JS_ENABLED = "isJsEnabled";
     String NOTIFICATION_ID_TAG = "wzrk_id";
     String DEEP_LINK_KEY = "wzrk_dl";
